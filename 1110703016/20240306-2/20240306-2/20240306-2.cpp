c9c1a17e857421b676ec286215c7bbb8c2b8bf6d
ponpongood <84927301+ponpongood@users.noreply.github.com>
2024-03-06 15:42:28 +0800

    Add files via upload

---
+++ 1110703016/20240306-2/20240306-2/20240306-2.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	float m, bmi;
+	int kg;
+	cout << "身高體重:";
+	cin >> m >> kg;
+	bmi = kg / (m * m);
+	cout << "BMI:" << bmi;
+	return 0;
+}
