2bbfc9e27affbcc80ac78839a70145f7bcd16bc5
張博鈞 <80932235+2003-David@users.noreply.github.com>
2024-03-06 15:47:42 +0800

    Add files via upload

---
+++ 1110831032/第一周.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    double height, weight, bmi;
+
+    cout << "請輸入您的身高（公尺）：";
+    cin >> height;
+
+    cout << "請輸入您的體重（公斤）：";
+    cin >> weight;
+
+    bmi = weight / (height * height);
+
+    cout << "您的BMI值為：" << bmi << endl;
+
+    if (bmi < 18.5) {
+        cout << "您的體重過輕。" << endl;
+    }
+    else if (bmi < 24) {
+        cout << "您的體重正常。" << endl;
+    }
+    else if (bmi < 27) {
+        cout << "您的體重過重。" << endl;
+    }
+    else {
+        cout << "您的體重肥胖。" << endl;
+    }
+
+    return 0;
+}
---
+++ 1110831032/第一周第二題.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int totalAmount;
+    cout << "請輸入您的消費金額：";
+    cin >> totalAmount;
+
+    int giftAmount = (totalAmount / 10000) * 1000;
+    cout << "根據您的消費金額，您可以獲得" << giftAmount << "元禮金。" << endl;
+
+    return 0;
+}
