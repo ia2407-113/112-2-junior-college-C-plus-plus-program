1ea100340f09bf1d1788096541b72b07336320d5
Qixuan0116 <144888878+Qixuan0116@users.noreply.github.com>
2024-03-06 15:50:38 +0800

    Add files via upload

---
+++ 1111134039/0306/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+﻿// ConsoleApplication1.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+#include<string>
+#include <iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    int a;
+    cout << "買多少";
+    cin >> a;
+    cout << a / 10;
+}
