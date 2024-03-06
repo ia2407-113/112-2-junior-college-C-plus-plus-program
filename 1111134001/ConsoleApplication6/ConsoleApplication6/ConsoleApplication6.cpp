80dc506e20230775f487498daae864a13a7d0acc
ccy0118 <144888816+ccy0118@users.noreply.github.com>
2024-03-06 15:43:40 +0800

    BMI
    BMI

---
+++ 1111134001/ConsoleApplication6/ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    double m, kg;
+    cout << "輸入身高和體重:";
+    cin >> m >> kg;
+    cout << "身高為" << m
+         << "體重為" << kg;
+    cout.precision(4);
+    double BMI = kg / m / m;
+    cout << "的BMI為=" << BMI;
+
+    return 0;
+}
