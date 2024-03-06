e67ca5320822348b0c7e1958cb714e9044696f6e
Rodtls <144888506+Rodtls@users.noreply.github.com>
2024-03-06 15:42:32 +0800

    Add files via upload

---
+++ 1111134012/0306/0306-1/0306/0306.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	int money;
+	cout << ("輸入買了多少錢:");
+	cin >> money;
+	cout << "獲得禮卷為" << (money / 10000) * 1000 << "元";
+
+	return 0;
+}
---
+++ 1111134012/0306/0306-2/0306-2/0306-2.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	double kg, m;
+	cout << "輸入體重(kg)與身高(m)(以空格隔開):";
+	cin >> kg >> m;
+	cout << "BMI為" << kg/(m * m);
+
+	return 0;
+}
+
