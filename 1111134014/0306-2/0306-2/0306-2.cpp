96545e89494ee43b871c6cb5b4defb9d595732cb
duyirou <144888576+duyirou@users.noreply.github.com>
2024-03-06 15:43:37 +0800

    Add files via upload

---
+++ 1111134014/0306-2/0306-2/0306-2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	float kg, m, bmi;  
+	cout << "輸入您的體重(公斤為單位)";
+	cin >> kg;
+	cout << "輸入您的身高(公尺為單位)";
+	cin >> m;
+	cout << "您的體重為" << kg << ",身高為" << m
+		<< "BMI為\t" << kg / (m * m);
+	return 0;
+}
