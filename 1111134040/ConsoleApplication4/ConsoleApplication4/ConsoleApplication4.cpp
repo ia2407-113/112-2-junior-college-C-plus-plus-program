0789239eaa7d7a1fc2cd86bf3f6589b3119a655a
voguevity <144888849+voguevity@users.noreply.github.com>
2024-03-06 15:42:56 +0800

    Add files via upload

---
+++ 1111134040/ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,15 @@
+﻿// ConsoleApplication4.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int money, sale;
+    cout << "輸入購物金額:";
+    cin >> money;
+    cout << "購物金額" << money
+        << ",禮卷有" << money/10000*1000;
+
+    return 0;
+}
---
+++ 1111134040/ConsoleApplication5/ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,17 @@
+﻿// ConsoleApplication5.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    float m, kg;
+    cout << "輸入身高與體重:";
+    cin >> m >> kg;
+    cout << "身高為" << m
+        << ",體重為" << kg
+        << ",BMI為=" << kg / (m*m);
+
+    return 0;
+}
