e64a4b62b15b836c0bedf41aef18c1ca7ef4be33
cheseees <144889454+cheseees@users.noreply.github.com>
2024-03-06 15:42:58 +0800

    Add files via upload

---
+++ 1111134022/0306 022-1/0306 022-1/0306 022-1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	float kg, m, Bmi;
+	cout << "輸入體重:";
+	cin >> kg;
+	cout << "輸入身高(公尺為單位):";
+	cin >> m;
+	cout << "Bmi為" << (kg / (m * m));
+	return 0;
+
+
+}
---
+++ 1111134022/0306 022-2/0306 022-2/0306 022-2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+	int X;
+	cout << "購買金額";
+	cin >> X;
+	cout << "禮卷金額=" << (X / 10);
+	return 0;
+
+
+
+}
