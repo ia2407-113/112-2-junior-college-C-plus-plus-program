7121dd4441b7201404ec35cbf4617308cb47da0d
kyoki <135029618+kyoki618@users.noreply.github.com>
2024-03-06 15:30:54 +0800

    Add files via upload

---
+++ 1110834023/1110834023-2/1110834023-2/1110834023-2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main() {
+    std::cout << "輸入身高（公分）: ";
+    double height;
+    std::cin >> height;
+
+    std::cout << "輸入體重（公斤）: ";
+    double weight;
+    std::cin >> weight;
+
+    double bmi = weight / ((height / 100) * (height / 100));
+
+    std::cout << "BMI為: " << bmi << std::endl;
+
+    return 0;
+}
---
+++ 1110834023/1110834023/1110834023/1110834023.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main() {
+
+    std::cout << "請輸入消費額: ";
+    int money;
+    std::cin >> money;
+
+    double feedback = 0;
+    if (money >= 10000) {
+        feedback = (money / 10000) * 1000;
+    }
+
+    std::cout << "您的反饋額為: " << feedback << " 元" << std::endl;
+
+    return 0;
+}
