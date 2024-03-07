d8aaee3905b38a34f4a18d6f7ca35733929cbbd7
Louis542466 <144888618+Louis542466@users.noreply.github.com>
2024-03-07 23:59:25 +0800

    0306作業

---
+++ 1111134044/ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    float weight, height;
+    cout << ("輸入身高(m)");
+    cin >> height;
+    cout << ("輸入體重(kg)");
+    cin >> weight;
+    cout << "BMI為" << (weight / (height * height));
+    return 0;
+}
