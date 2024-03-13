ed3aa316f98a6d2860554f2c508805df834be12d
tutjui7y6i <144888790+tutjui7y6i@users.noreply.github.com>
2024-03-13 15:53:18 +0800

    Add files via upload

--- 1111134033/1111134033-2/1111134033-2/1111134033-2.cpp
+++ 1111134033/1111134033-2/1111134033-2/1111134033-2.cpp
@@ -3,16 +3,25 @@
 
 #include <iostream>
 using namespace std;
-
 int main()
 {
-    float weight, height;
-    cout << "輸入體重及身高:";
-    cin >> weight >> height;
-    cout << "體重" << weight << ",身高" << height
-        << "的BMI值=" << weight / (height * height);
-    return 0;
+	int a, b, c;
+	cout << "輸入邊長abc  ";
+	cin >> a >> b >> c;
+	if (a + b <= c)
+		cout << "這三邊長無法成為三角形";
+
+	else if (a + c <= b)
+		cout << "這三邊長無法成為三角形";
+
+	else if (b + c <= a)
+		cout << "這三邊長無法成為三角形";
+
+	else
+		cout << "可成一組三角形";
+	return 0;
 }
+
 // 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
 // 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
 
