f9578c01d8b6e77edd85cb684f976fee293f5dd5
Cyzz11 <144888845+Cyzz11@users.noreply.github.com>
2024-03-13 15:51:50 +0800

    3/13

--- 1111134016/1111134016/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ 1111134016/1111134016/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,13 +5,36 @@
 using namespace std;
 int main()
 {
-	float kg, m;
-	cout << ("輸入體重與身高(身高請用公尺輸入並與體重用空白隔開):");
-	cin >> kg >> m;
-	cout << "BMI值為" << (kg/(m*m));
+	float money;
+	cout << "綜合所得淨額=";
+	cin >> money;
+	if (money <= 560000)
+		cout << "為5%級距,應納稅額=" << money * 0.05;
+	else if (money >= 560001 && money <= 1260000)
+		cout << "為12%級距,應納稅額=" << money * 0.12 - 39200;
+	else if (money >= 1260001 && money <= 2520000)
+		cout << "為20%級距,應納稅額=" << money * 0.2 - 140000;
+	else if (money >= 2520001 && money <= 4720000)
+		cout << "為30%級距,應納稅額=" << money * 0.3 - 392000;
+	else if (money >= 4720001)
+		cout << "為40%級距,應納稅額=" << money * 0.4 - 864000;
+	
 
+
+	int a, b, c;
+	cout << "\n輸入三角形的三個邊長(以空白隔開)";
+	cin >> a >> b >> c;
+	if ((a + b) <= c ) 
+		cout << "無法構成三角形";
+		else if((a + c )<= b || (b + c) <= a)
+		cout << "無法構成三角形";
+		else
+		cout << "可構成三角形";
+			
 	return 0;
 }
+
+
 
 // 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
 // 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
