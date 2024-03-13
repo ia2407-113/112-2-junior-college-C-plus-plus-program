c19bab0ed50954c8f4cadd1257de002bf57a03e6
Zehong960806 <144889047+Zehong960806@users.noreply.github.com>
2024-03-13 15:03:18 +0800

    Add files via upload

---
+++ 1111134045/0313(1)/0313(1)/0313(1).cpp
@@ -0,0 +1,15 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a=0, b=0, c =0;
+    cout << "請輸入三角形a,b,c的邊長(用空格分開)";
+	cin >>a >> b>> c;
+	if (a+b<=c || a+c<=b || b+c<=a)
+		cout << "無法成為三角形";
+	else
+		cout << "可以成為三角形";
+}
+
