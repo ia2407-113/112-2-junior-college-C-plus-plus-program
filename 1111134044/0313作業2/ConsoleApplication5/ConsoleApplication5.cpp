a3b31dcc984bcace01f36ec2001c6c49592cf958
Louis542466 <144888618+Louis542466@users.noreply.github.com>
2024-03-13 15:37:10 +0800

    0313

---
+++ 1111134044/0313作業2/ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,22 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b, c;
+	cout << "輸入邊長abc  ";
+	cin >> a >> b >> c;
+	if (a + b <= c)
+		cout << "這三邊長無法成為三角形";
+
+	else if (a + c <= b)
+		cout << "這三邊長無法成為三角形";
+
+	else if (b + c <= a)
+		cout << "這三邊長無法成為三角形";
+
+	else 
+		cout << "可成一組三角形";
+	return 0;
+}
+
