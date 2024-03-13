4ffe813199f80303d3d94f4c5ac637306f4c0f30
allenexx <80932266+allenexx@users.noreply.github.com>
2024-03-13 15:37:58 +0800

    Add files via upload

---
+++ 1110831044/三角形/1110831044-1/1110831044-1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    double a, b, c;
+
+    cout << "請輸入三角形三邊常(以空格隔開)：";
+    cin >> a >> b >> c;
+
+    if (a<=0 || b<=0 || c<=0) {
+        cout << "邊長必為正數" << endl;
+    }
+    else if ((a + b) <= c || (a + c) <= b || (b + c) <= a) {
+        cout << "無法構成一個三角形。" << endl;
+    }
+    else{
+        cout << "可以構成一個三角形。" << endl;
+    }
+
+    return 0;
+}
---
+++ 1110831044/稅率/1110831044/1110831044.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+
+double calculate_tax(double income, double& tax_rate) {
+    double progressive_difference;
+    double tax_amount;
+
+    // 根据综合所得的不同范围，选择相应的税率和累进差额
+    if (income <= 560000) {
+        tax_rate = 0.05;
+        progressive_difference = 0;
+    }
+    else if (income <= 1260000) {
+        tax_rate = 0.12;
+        progressive_difference = 39200;
+    }
+    else if (income <= 2520000) {
+        tax_rate = 0.20;
+        progressive_difference = 140000;
+    }
+    else if (income <= 4720000) {
+        tax_rate = 0.30;
+        progressive_difference = 392000;
+    }
+    else {
+        tax_rate = 0.40;
+        progressive_difference = 864000;
+    }
+
+    // 计算应纳税额
+    tax_amount = income * tax_rate - progressive_difference;
+    // 如果应纳税额为负数，则设为0
+    tax_amount = max(tax_amount, 0.0);
+    return tax_amount;
+}
+
+int main() {
+    double income;
+    cout << "請輸入綜合所得:：";
+    cin >> income;
+
+    double tax_rate;
+    double tax_amount = calculate_tax(income, tax_rate);
+    cout << "您的應納稅額:" << tax_amount << endl;
+    cout << "對應的稅率:" << tax_rate * 100 << "%" << endl;
+
+    return 0;
+}
