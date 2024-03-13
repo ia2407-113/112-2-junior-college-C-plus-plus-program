f33f593152df24e71d5d991ba493cfe2396e8826
kyoki <135029618+kyoki618@users.noreply.github.com>
2024-03-13 15:34:52 +0800

    Add files via upload

---
+++ 1110834023/0313-2/ConsoleApplication5/ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main() {
+    double a, b, c;
+    std::cout << "請輸入三邊長 a, b, c: ";
+    std::cin >> a >> b >> c;
+
+    if (a + b <= c || a + c <= b || b + c <= a) {
+        std::cout << "無法構成一個三角形" << std::endl;
+    }
+    else {
+        std::cout << "可以構成一個三角形" << std::endl;
+    }
+
+    return 0;
+}
---
+++ 1110834023/0313/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+
+int main() {
+    double income;
+    std::cout << "請輸入綜合所得淨額: ";
+    std::cin >> income;
+    int tax = 0;
+
+    std::string range;
+
+    if (income <= 560000) {
+        tax = income * 0.05 - 0;
+        range = "0 ~ 560,000";
+    }
+    else if (income <= 1260000) {
+        tax = income * 0.12 - 39200;
+        range = "560,001 ~ 1,260,000";
+    }
+    else if (income <= 2520000) {
+        tax = income * 0.20 - 140000;
+        range = "1,260,001 ~ 2,520,000";
+    }
+    else if (income <= 4720000) {
+        tax = income * 0.30 - 392000;
+        range = "2,520,001 ~ 4,720,000";
+    }
+    else {
+        tax = income * 0.40 - 864000;
+        range = "4,720,001 以上";
+    }
+
+    std::cout << "應納稅額為: " << tax << " 元" << std::endl;
+    std::cout << "級距: " << range << std::endl;
+
+    return 0;
+}
