b73c206be1dc6a37b18f07217bbc2888ae2774f8
cheseees <144889454+cheseees@users.noreply.github.com>
2024-03-13 15:21:59 +0800

    Add files via upload

---
+++ 0313 022-2/0313 022-2/0313 022-2.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a,b,c = 0;
+	cout << "輸入三角形的邊(空格隔開)";
+	cin >> a >> b >> c;
+	if (a + b <= c)
+	{
+		cout << "無法構成一個三角形";
+	}
+	else if (a + c <= b)
+	{
+		cout << "無法構成一個三角形";
+	}
+	else if (b + c <= a)
+	{
+		cout << "無法構成一個三角形";
+	}
+	else {
+		cout << "可以構成一個三角形";
+	}
+
+
+	
+
+}
+
