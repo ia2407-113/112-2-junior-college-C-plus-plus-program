4ec0f578f791a31706f8a5529a695c79573a3835
07158yunxin <144889045+07158yunxin@users.noreply.github.com>
2024-03-20 16:12:34 +0800

    Add files via upload

---
+++ 1111134011/1111134011.3.20-3/1111134011.3.20-3/1111134011.3.20-3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+    int password;
+    int attempts = 0; 
+
+    do {
+        cout << "請輸入密碼: ";
+        cin >> password;
+
+        if (password != 123) {
+           cout << "密碼輸入錯誤" <<"\n";
+            attempts++;
+
+            if (attempts == 3) {
+                cout << "暫停使用本系統!" << "\n";
+                return 0; 
+            }
+        }
+        else {
+            cout << "歡迎光臨本系統!" << "\n";
+            break; 
+        }
+
+    } while (true);
+
+    return 0;
+}
