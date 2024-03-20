644b672ce3a91f6ba186927f6db123f6e3d08bfa
Zehong960806 <144889047+Zehong960806@users.noreply.github.com>
2024-03-20 15:42:07 +0800

    Add files via upload

---
+++ 1111134045/0320(2)/0320(2)/0320(2).cpp
@@ -0,0 +1,30 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a,count=1;
+	cout << "請輸入密碼:";
+	cin >> a;	
+	do {
+		if (a == 123)
+		{
+			cout << "歡迎光臨本系統!";
+			count = 4;
+		}
+		else
+		{
+			cout << "密碼輸入錯誤\n";
+			cout << "請輸入密碼:";
+			cin >> a;
+		}
+		count += 1;
+		if (count == 3)
+		{
+			cout << "暫停使用本系統!";
+		}
+	} while (count < 3);
+}
+
+
