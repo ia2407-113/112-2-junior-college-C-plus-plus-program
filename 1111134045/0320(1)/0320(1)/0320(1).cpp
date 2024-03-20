a9904fd847937d839b9b7bb4306d79592a4997f4
Zehong960806 <144889047+Zehong960806@users.noreply.github.com>
2024-03-20 15:41:18 +0800

    Add files via upload

---
+++ 1111134045/0320(1)/0320(1)/0320(1).cpp
@@ -0,0 +1,17 @@
+﻿// 0320(1).cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	for (int x=5; x>=1; x--)
+	{
+		for (int a = 1; a<=((x*2)-1); a++)
+		{
+			cout << a;
+		}
+		cout << "\n";
+	}
+}
+
