894c58cab7125b0ab3cd0ae535d6820e6d889664
ccy0118 <144888816+ccy0118@users.noreply.github.com>
2024-03-20 16:05:43 +0800

    03.20
    第三題

---
+++ 1111134001/ConsoleApplication(three)/ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	int num = 1, password;
+	do
+	{
+		cout << "輸入密碼:";
+		cin >> password;
+		if (password == 123)
+		{
+			cout << "密碼正確!歡迎光臨本系統~";
+			break;
+		}
+		else
+			cout << "密碼錯誤\n";
+		num = num + 1;
+	} while (num <= 3);
+	cout << "暫停使用本系統\n";
+	return  0;
+}
+
