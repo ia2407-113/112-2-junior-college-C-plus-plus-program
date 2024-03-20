3bfa7a26c7a0898e97579401b5742bfac8bfff11
voguevity <144888849+voguevity@users.noreply.github.com>
2024-03-20 15:32:17 +0800

    Add files via upload

---
+++ 1111134040/輸入密碼/ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,17 @@
+﻿// ConsoleApplication5.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+
+int main() {
+    int rows = 5; // 控制行數
+
+    for (int i = 1; i <= rows; ++i) {
+        for (int j = 1; j <= (2 * rows - 2 * i + 1); ++j) {
+            std::cout << j;
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
