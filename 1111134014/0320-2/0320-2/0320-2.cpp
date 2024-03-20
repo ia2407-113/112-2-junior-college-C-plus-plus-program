d2a2ead7732093e9e02bfaf67b991e1017708d7e
duyirou <144888576+duyirou@users.noreply.github.com>
2024-03-20 16:07:27 +0800

    印出數字三角形

---
+++ 1111134014/0320-2/0320-2/0320-2.cpp
@@ -0,0 +1,17 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	for (int i = 1; i <=9; i++)//直行的計算
+	{
+		for (int j = 1; j <= 9-(i-1)*2 ; j++)//橫列的計算 
+		{
+			cout << j;
+		}
+		cout << "\n";
+	}
+	return 0;
+}
