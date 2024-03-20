80e78db4725b70445a6a9309a79775b5cae09014
ccy0118 <144888816+ccy0118@users.noreply.github.com>
2024-03-20 16:05:00 +0800

    03.20
    第二題

---
+++ 1111134001/ConsoleApplication(two)/ConsoleApplication6/ConsoleApplication6.cpp
@@ -0,0 +1,18 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	for (a = 9; a >= 1; a = a - 2)
+	{
+		for(b=1;b<=a;b++)
+		{
+			cout << b;
+		}
+		cout << "\n";
+	}
+	return 0;
+}
+
