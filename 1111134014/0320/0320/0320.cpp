491897a340939c3d8ec56db0df7f42c83e067629
duyirou <144888576+duyirou@users.noreply.github.com>
2024-03-20 16:06:41 +0800

    輸入小於50的整數

---
+++ 1111134014/0320/0320/0320.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double num , i ,sum;
+    sum = 0;
+    cout << "請輸入小於50的數字";
+    cin >> num;
+    if (num > 50)
+        cout << "數字大於50"; 
+    else
+    {
+        for (i = 1; i <= num; i++)//前測迴圈
+       { 
+        sum += (1 / i); 
+       }  
+    }  
+    cout << "和為" << sum;
+}
+
+
