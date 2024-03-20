40d1be372f38a7843f1399ef516d1cb077c0ff0e
Zehong960806 <144889047+Zehong960806@users.noreply.github.com>
2024-03-20 15:40:35 +0800

    Add files via upload

---
+++ 1111134045/0320/0320/0320.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    double sum = 1;
+    cout << "請輸入數字(小於50):";
+    cin >> a;
+    if (a<50)
+    {
+        for (double x=2;x<=a ; x++)
+            sum = sum + (1 / x);
+        cout << sum;
+    }
+    else
+    {
+        cout <<"輸入小於50的數字!";
+    }
+    
+}
+
+
