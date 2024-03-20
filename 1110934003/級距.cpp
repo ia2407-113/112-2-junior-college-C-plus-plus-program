6a21101e391682dc126dcffb9160adbe30b4e30d
Liimaomao <160704349+Liimaomao@users.noreply.github.com>
2024-03-20 15:36:00 +0800

    Add files via upload

---
+++ 1110934003/級距.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    double a, b, c;
+
+    // 輸入三角形的三邊長
+    cout << "請輸入三角形的三邊長：\n";
+    cout << "a: ";
+    cin >> a;
+    cout << "b: ";
+    cin >> b;
+    cout << "c: ";
+    cin >> c;
+
+    // 判斷是否可以構成三角形
+    if (a + b <= c || a + c <= b || b + c <= a) { // 若任意兩邊之和小於等於第三邊，則無法構成三角形
+        cout << "無法構成一個三角形。\n";
+    }
+    else {
+        cout << "可以構成一個三角形。\n";
+    }
+
+    return 0;
+}
