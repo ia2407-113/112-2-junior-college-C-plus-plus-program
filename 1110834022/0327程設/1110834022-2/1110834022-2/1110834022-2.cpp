8423e59cd454cc6470f9cc321c5f1dde48c0ef82
Aron23456 <137755615+Aron23456@users.noreply.github.com>
2024-03-27 15:32:32 +0800

    Add files via upload

---
+++ 1110834022/0327程設/1110834022-2/1110834022-2/1110834022-2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    string str;
+    getline(cin, str);
+
+    // 將所有字元改成小寫
+    transform(str.begin(), str.end(), str.begin(), ::tolower);
+
+    // 將每個字的第一個字母改成大寫
+    for (int i = 0; i < str.size(); ++i) {
+        if (i == 0 || str[i - 1] == ' ') {
+            str[i] = toupper(str[i]);
+        }
+    }
+
+    cout << str << endl;
+
+    return 0;
+}
