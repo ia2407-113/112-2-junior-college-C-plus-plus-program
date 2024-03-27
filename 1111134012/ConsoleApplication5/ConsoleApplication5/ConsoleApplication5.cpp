b08071fb4b53f8bd58b6d1695024e21bdb5e796c
Rodtls <144888506+Rodtls@users.noreply.github.com>
2024-03-27 15:56:44 +0800

    Add files via upload

---
+++ 1111134012/ConsoleApplication5/ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cctype> // 包含這個標頭文件以使用 toupper 函數
+
+using namespace std;
+
+int main() {
+    string sentence;
+    cout << "請輸入一句英文: ";
+    cin >> sentence;
+
+    // 將第一個字母改成大寫
+    for (size_t i = 0; i < sentence.length(); ++i) {
+        if (i == 0 || isspace(sentence[i - 1])) {
+            sentence[i] = toupper(sentence[i]);//toupper是為了把字母轉成大寫
+        }
+    }
+
+    // 輸出結果
+    cout << "處理後的句子: " << sentence << endl;
+
+    return 0;
