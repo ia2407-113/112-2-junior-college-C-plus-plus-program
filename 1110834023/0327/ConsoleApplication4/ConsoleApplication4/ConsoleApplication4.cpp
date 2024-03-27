2e47a77ace6af0c6238b2c15551d22f80480b161
kyoki <135029618+kyoki618@users.noreply.github.com>
2024-03-27 15:58:49 +0800

    Add files via upload

---
+++ 1110834023/0327/ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+
+int main() {
+    std::string line;
+    std::cout << "請輸入: ";
+    std::getline(std::cin, line);
+    bool isFirst = true;
+    for (char& c : line) {
+        if (isFirst && std::isalpha(c)) {
+            c = std::toupper(c);
+            isFirst = false;
+        }
+        else if (!std::isalpha(c)) {
+            isFirst = true;
+        }
+    }
+    std::cout << line << std::endl;
+    return 0;
+}   
---
+++ 1110834023/0327/ConsoleApplication5/ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+
+int main() {
+    std::string input;
+    std::map<char, int> charCount;
+    std::cout << "請輸入 ";
+    std::getline(std::cin, input);
+    for (char c : input) {
+        if (c >= 32 && c <= 127) {
+            charCount[c]++;
+        }
+    }
+    for (const auto& pair : charCount) {
+        std::cout << pair.first << " 出現的次數為: " << pair.second << std::endl;
+    }
+    return 0;
+}
