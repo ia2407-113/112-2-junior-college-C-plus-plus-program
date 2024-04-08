5c7c4200022923fd5506a50f81d12fb491e15793
Rodtls <144888506+Rodtls@users.noreply.github.com>
2024-04-08 12:08:59 +0800

    Add files via upload

---
+++ 1111134012/0327作業/2.身分證字號/2/2.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string a, b;
+    int len1, len2;
+
+
+    cout << "輸入第一個身分證字號：";
+    cin >> a;
+
+
+    cout << "輸入第二個身分證字號：";
+    cin >> b;
+
+    len1 = a.length(); //存取身分證字號的長度
+    len2 = b.length();
+
+
+    if (len1 != 10)// 檢查第一個身分證字號是否為十碼
+        cout << "輸入的身分證字號錯誤";
+    else {
+        if (a[1] == '1')// 辨識第一個身分證字號的性別 第二位
+            cout << "第一個身分證為男性\n";
+
+        else if (a[1] == '2')
+            cout << "第一個身分證為女性\n";
+
+        else
+            cout << "第一個身分證字號錯誤\n";
+    }
+
+
+    if (len2 != 10)// 檢查第二個身分證字號是否為十碼
+        cout << "輸入的身分證字號錯誤\n";
+    else {
+        if (b[1] == '1')// 辨識二號身分證字號的性別
+            cout << "第二個身分證為男性\n";
+        else if (b[1] == '2')
+            cout << "第二個身分證為女性\n";
+        else
+            cout << "第二個身分證字號錯誤\n";
+    }
+
+    if (a.substr(2, 7) < b.substr(2, 7))// 3~6 // 使用 substr() 
+        cout << "第一個年紀比較大";
+    else if (a.substr(2, 7) > b.substr(2, 7))
+        cout << "第二個年紀比較大";
+
+    return 0;
+}
---
+++ 1111134012/0327作業/3.朋友和電話號碼/3/3.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    const int numFriends = 5;
+    string names[numFriends];
+    string phones[numFriends];
+
+    // 輸入朋友的姓名和電話號碼
+    for (int i = 0; i < numFriends; ++i) {
+        cout << "請輸入第" << i + 1 << "個朋友的姓名:";
+        getline(cin, names[i]);
+        cout << "請輸入第" << i + 1 << "個朋友的電話號碼:";
+        getline(cin, phones[i]);
+    }
+
+    // 輸入要查詢的朋友姓名
+    string searchName;
+    cout << "請輸入要查詢的朋友姓名:";
+    getline(cin, searchName);
+
+    // 在已知朋友姓名中查找對應的電話號碼
+    bool found = false;
+    for (int i = 0; i < numFriends; ++i) {
+        if (names[i] == searchName) {
+            cout << "朋友 " << searchName << " 的電話號碼是:" << phones[i] << endl;
+            found = true;
+            break;
+        }
+    }
+
+    // 如果未找到朋友
+    if (!found) {
+        cout << "找不到朋友 " << searchName << " 的信息。" << endl;
+    }
+
+    return 0;
+}
