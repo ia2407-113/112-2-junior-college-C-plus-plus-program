1e7e7a32d6bd1f4c9de3ed957a5e238bdc15a580
Sinyuuuu <144888565+Sinyuuuu@users.noreply.github.com>
2024-04-10 01:26:38 +0800

    Add files via upload

---
+++ 1111134010/W6/第六週作業/第一題/第一題.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str1;
+    getline(cin, str1);
+    int a;
+    a = str1.length();
+    for (size_t i = 1; i < a; i++)
+    {
+        str1[i] = toupper(str1[i]);
+    }
+    for (size_t i = 0; i < a; i++)
+    {
+        if (str1[i] == ' ')
+            str1[i + 1] = tolower(str1[i + 1]);
+    }
+    cout << str1;
+}
