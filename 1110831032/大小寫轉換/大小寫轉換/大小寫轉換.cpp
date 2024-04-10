f65fdc0b867b733c4d5eabdf54a765ac4f2e77a8
張博鈞 <80932235+2003-David@users.noreply.github.com>
2024-04-10 13:16:02 +0800

    Add files via upload

---
+++ 1110831032/大小寫轉換/大小寫轉換/大小寫轉換.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str1;
+    getline(cin, str1);//輸入一句話英文
+    int a;
+    a = str1.length();
+    for (int i = 0; i < a; i++)
+    {
+        str1[i] = toupper(str1[i]);//小寫改大寫
+    }
+    str1[0] = tolower(str1[0]);
+    for (int i = 0; i < a; i++)
+    {
+        if (str1[i] == ' ') //找空白
+            str1[i + 1] = tolower(str1[i + 1]);//大寫改小寫
+    }
+    cout << str1;
+}
---
+++ 1110831032/電話號碼/電話號碼/電話號碼.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    const int numFriends = 5;
+    string names[numFriends];
+    string phones[numFriends];
+
+    // 輸入朋友的姓名和電話
+    for (int i = 0; i < numFriends; ++i) {
+        cout << "請輸入第 " << (i + 1) << " 個朋友的姓名：";
+        getline(cin, names[i]);
+
+        cout << "請輸入第 " << (i + 1) << " 個朋友的電話：";
+        getline(cin, phones[i]);
+    }
+
+    // 輸入要查詢的朋友姓名
+    string queryName;
+    cout << "請輸入要查詢的朋友的姓名：";
+    getline(cin, queryName);
+
+    // 尋找朋友並輸出電話
+    bool found = false;
+    for (int i = 0; i < numFriends; ++i) {
+        if (names[i] == queryName) {
+            cout << "朋友 " << queryName << " 的電話是：" << phones[i] << endl;
+            found = true;
+            break;
+        }
+    }
+
+    if (!found) {
+        cout << "找不到名為 " << queryName << " 的朋友。" << endl;
+    }
+
+    return 0;
+}
