bd3cf2728934acd30dd7dc5de5b0dfba7185b126
113ia2407 <113ia2407@gmail.com>
2024-04-10 15:02:42 +0800

    set

---
+++ week03/範例4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std ;
+int main()
+ {  
+   int length=20, width=12 ;  //  長方形的長與寬
+   cout << "長為" << length << ",寬為" << width
+        << "的長方形面積=" << length * width ;  
+
+   return 0;
+ }
---
+++ week03/範例5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std ;
+int main()
+ {
+   int length,width;
+   cout << "輸入長方形的長與寬:" ;
+   cin >> length >> width;
+   cout << "長為" << length 
+        << ",寬為" << width
+        << "的長方形面積=" << length * width ; 
+  
+   return 0;
+ }
