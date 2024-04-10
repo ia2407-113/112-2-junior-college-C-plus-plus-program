eacff04f5dc68b78b71bdc90c3257e336bb81cac
Qixuan0116 <144888878+Qixuan0116@users.noreply.github.com>
2024-04-10 13:14:36 +0800

    Add files via upload

---
+++ 1111134039/作業01/ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <cctype>
+#include<string>
+#include <numbers>
+using namespace std;
+int main()
+{
+	string str1, str2, id1, id2, id3, id4;
+	string g = "1";
+	int pos = 1;//取第幾個字
+	int n = 1;//取多少字
+	int r = 2;
+	int a = 7;
+	cout << "輸入身分證字號(要輸英文)";
+	getline(cin, str1);
+	cout << "輸入身分證字號(要輸英文)";
+	getline(cin, str2);
+	id1 = str1.substr(pos, n);
+	id2 = str2.substr(pos, n);//substr是取
+	id3 = str1.substr(r, a);
+	id4 = str2.substr(r, a);
+	if (id1 == g)
+		cout << str1 << "是男的\n";
+	else
+		cout << str1 << "是女的\n";
+	if (id2 == g)
+		cout << str2 << "是男的\n";
+	else
+		cout << str2 << "是女的\n";
+
+	if (id3 < id4)
+		cout << str1 << "比較大\n";
+	else
+		cout << str2 << "比較大\n";
+}
---
+++ 1111134039/作業01/ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,25 @@
+﻿﻿// 非首字大寫.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str1;
+    getline(cin, str1);
+    int a;
+    a = str1.length();
+    for (int i = 0; i < a; i++)
+    {
+        str1[i] = toupper(str1[i]);
+    }
+    str1[0] = tolower(str1[0]);
+    for (int i = 0; i < a; i++)
+    {
+        if (str1[i] == ' ')
+            str1[i + 1] = tolower(str1[i + 1]);
+    }
+    cout << str1;
+}
