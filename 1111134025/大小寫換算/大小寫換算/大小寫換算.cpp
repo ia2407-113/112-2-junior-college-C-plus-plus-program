aa8f4688a3858999074ad4a21294ce79cb3edbe9
ADIJER <144888843+Saiaserd@users.noreply.github.com>
2024-04-10 14:09:42 +0800

    Add files via upload

---
+++ 1111134025/大小寫換算/大小寫換算/大小寫換算.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+#include <cctype> // For std::toupper
+using namespace std;
+
+int main() {
+    string str1;
+    int i = 0;
+    cout << "請輸入一句英文：";
+    getline(cin, str1);
+
+    do
+    {
+        if (isalnum(str1[i]))
+        {
+            if (i == 0 || str1[i - 1] == ' ') //當字串是第一個字或是空格後的第一個字時不用變化
+            {
+            }
+            else
+            {
+                str1[i] = toupper(str1[i]);
+            }
+        }
+
+        i++;
+    } while (i < str1.length());
+
+    cout << str1;
+
+    return 0;
+}
