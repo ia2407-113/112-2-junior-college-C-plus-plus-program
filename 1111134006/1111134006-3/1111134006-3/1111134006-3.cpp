650b16c60ffaa8a6e23f0d6a6a37c6c4e0505860
1111134006 <144889442+1111134006@users.noreply.github.com>
2024-04-10 13:44:57 +0800

    0327-3

--- 1111134006/1111134006-3/1111134006-3/1111134006-3.cpp
+++ 1111134006/1111134006-3/1111134006-3/1111134006-3.cpp
@@ -1,35 +1,61 @@
 ﻿// 1111134006-3.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
 //
-
 #include <iostream>
 using namespace std;
 int main()
 {
-	int num = 1, password, max = 3;
-	do
-	{
-		cout << "輸入密碼:";
-		cin >> password;
-		if (password == 123)
-		{
-			cout << "歡迎光臨本系統";
-			break;
-		}
-		else if (num >= max)
-		{
-			cout << "暫停使用本系統";
-			break;
-		}
-		else
-			cout << "密碼輸入錯誤\n";
-		num = num + 1;
 
-	} 
-	while (num <= 3);
-	return 0;
+    string id1, id2;
+    cout << "請輸入第一位的身分證:";
+    cin >> id1;
+    cout << "請輸入第二位的身分證:";
+    cin >> id2;
 
+    if (id1[1] == '1')
+    {
+        cout << id1 << "是男性";
+    }
+    else
+    {
+        cout << id1 << "是女性";
+    }
+    if (id2[1] == '1')
+    {
+        cout << id2 << "是男性";
+    }
+    else
+    {
+        cout << id2 << "是女性";
+    }
+    for (int i = 2; i <= 8; ++i)
+    {
+        if (id1[i] < id2[i]) {
+            if (id1[1] == '1')
+            {
+                cout << "第一位的男性年齡較大";
+            }
+            else
+            {
+                cout << "第一位的女性年齡較大";
+            }
+            break;
+        }
+        else if (id1[i] > id2[i]) {
+            if (id2[1] == '1')
+            {
+                cout << "第二位的男性年齡較大";
+            }
+            else
+            {
+                cout << "第二位的女性年齡較大";
+            }
+            break;
+        }
+
+    }
+
+    return 0;
 }
-
 // 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
 // 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
 
