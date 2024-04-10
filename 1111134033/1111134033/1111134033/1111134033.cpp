09e2d62e445f85b653e44edd4401d5ed9dce7628
tutjui7y6i <144888790+tutjui7y6i@users.noreply.github.com>
2024-04-10 16:05:18 +0800

    Add files via upload

--- 1111134033/1111134033/1111134033/1111134033.cpp
+++ 1111134033/1111134033/1111134033/1111134033.cpp
@@ -2,17 +2,41 @@
 //
 
 #include <iostream>
+#include <string>
 using namespace std;
-int main()
+void bubble(int arr[], int num);
+
+int main() {
+    int size[5];
+
+    for (int i = 0; i < 5; i++) {
+        cout << "請輸入數字";
+        cin >> size[i];
+
+    }
+    bubble(size, 5);
+    return 0;
+}
+
+void bubble(int arr[], int size)
 {
-    int i,a ;
-    double sum = 0;
-    cout << "輸入小於50的正整數n:";
-    cin >> a;
-    for (i = 1; i <=a; i++)
-        sum+= 1.0 / i;
-        cout << "1 + 1 / 2 + 1 / 3 + … + 1 / n之和=" << sum ;
-        return 0;
+    for (unsigned int pass = 1; pass < size; ++pass)
+    {
+        for (int i = 0; i < size - 1; ++i)
+        {
+            if (arr[i] > arr[i + 1])
+            {
+                int hold = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = hold;
+            }
+        }
+    }
+    cout << "\n資料項目排列後印出\n";
+    for (int g = 0; g < 5; g++)
+    {
+        cout << arr[g] << ' ';
+    }
 }
 
 // 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
