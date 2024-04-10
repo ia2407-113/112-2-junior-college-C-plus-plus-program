3378462fdfd7cab1428891f3233bef24b4196970
Cyzz11 <144888845+Cyzz11@users.noreply.github.com>
2024-04-10 16:51:00 +0800

    4/10

--- 1111134016/1111134016/ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
+++ 1111134016/1111134016/ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,56 +1,61 @@
-﻿// ConsoleApplication2.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
-//
-
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
+void volume(int a);//立方
+void volume(float a);//球體
+void volume(float a, float b);//圓柱
+void volume(float a, float b, float c);//長方
 int main()
 {
+    int h;
+    float v, s, k, t, d, l;
+    cout << "輸入立方體邊的長度:";
+    cin >> h;
+    volume(h);
 
-    string id1, id2;
-    cout << "請輸入第一位的身分證:";
-    cin >> id1;
-    cout << "請輸入第二位的身分證:";
-    cin >> id2;
+    cout << "輸入球體半徑:";
+    cin >> v;
+    volume(v);
 
-    for (int i = 2; i <= 8; ++i)// 迴圈比較兩位身分證字號的第 2 到第 8 碼
-    {
-        if (id1[i] < id2[i])
-        {
-            if (id1[1] == '1')// 判斷第一位身分證字號的性別
-            {
-                cout << "第一位的男性年齡較大";
-            }
-            else
-            {
-                cout << "第一位的女性年齡較大";
-            }
-            break;
-        }
-        else if (id1[i] > id2[i]) // 如果第一位的第 i 碼大於第二位的第 i 碼
-        {
-            if (id2[1] == '1')
-            {
-                cout << "第二位的男性年齡較大";
-            }
-            else
-            {
-                cout << "第二位的女性年齡較大";
-            }
-            break;
-        }
+    cout << "輸入圓柱體體積半徑:";
+    cin >> s;
+    cout << "輸入圓柱體體積高:";
+    cin >> k;
+    volume(s, k);
 
-    }
+    cout << "輸入長方體體積長:";
+    cin >> t;
+    cout << "輸入長方體體積寬:";
+    cin >> d;
+    cout << "輸入長方體體積高:";
+    cin >> l;
+    volume(t, d, l);
 
-    return 0;
 }
-
-// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
-// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
-
-// 開始使用的提示: 
-//   1. 使用 [方案總管] 視窗，新增/管理檔案
-//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
-//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
-//   4. 使用 [錯誤清單] 視窗，檢視錯誤
-//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
-//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
+void volume(int a)
+{
+    int q;
+    q = a * a * a;
+    cout << "立方體體積為:" << q;
+    cout << "\n";
+}
+void volume(float a)
+{
+    float w;
+    w = (4.0 / 3.0) * 3.14 * (a * a * a);
+    cout << "球體體體積為:" << w;
+    cout << "\n";
+}
+void volume(float a, float b)
+{
+    float p;
+    p = 3.14 * a * a * b;
+    cout << "圓柱體體體積為:" << p;
+    cout << "\n";
+}
+void volume(float a, float b, float c)
+{
+    float o;
+    o = a * b * c;
+    cout << "長方體體體積為:" << o;
+    cout << "\n";
+}
--- 1111134016/1111134016/ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
+++ 1111134016/1111134016/ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -1,38 +1,53 @@
-﻿// ConsoleApplication3.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
-//
+﻿#include <iostream>
 
-#include <iostream>
 using namespace std;
-int main()
-{
-    string name[5], phone[5], search;// 宣告兩個字串陣列
-    for (int i = 0; i < 5; i++)
-    {
-        cout << "請輸入第" << i + 1 << "位朋友的姓名:";
-        cin >> name[i];
-        cout << "請輸入第" << i + 1 << "位朋友的電話:";
-        cin >> phone[i];
-    }
-    cout << "請輸入要查詢的朋友之姓名:";
-    cin >> search;
-    for (int j = 0; j < 5; j++)
-    {
-        if (search == name[j])
-        {
-            cout << "他的電話是:" << phone[j];
-            return 0;
+
+// 定義自訂函式
+void swap(int* a, int* b) {
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+
+    // 定義5個整數變數
+    int num1, num2, num3, num4, num5;
+
+    // 輸入5個整數
+    cout << "請輸入第1個整數:";
+    cin >> num1;
+    cout << "請輸入第2個整數:";
+    cin >> num2;
+    cout << "請輸入第3個整數:";
+    cin >> num3;
+    cout << "請輸入第4個整數:";
+    cin >> num4;
+    cout << "請輸入第5個整數:";
+    cin >> num5;
+
+    // 使用自訂函式將5個整數排序
+    for (int i = 0; i < 4; i++) {
+        for (int j = i + 1; j < 5; j++) {
+            if (num1 > num2) {
+                swap(&num1, &num2);
+            }
+            if (num2 > num3) {
+                swap(&num2, &num3);
+            }
+            if (num3 > num4) {
+                swap(&num3, &num4);
+            }
+            if (num4 > num5) {
+                swap(&num4, &num5);
+            }
         }
     }
-    cout << "查無資料";
+
+    // 輸出排序後的5個整數
+    cout << "排序後的5個整數為:";
+    cout << num1 << " " << num2 << " " << num3 << " " << num4 << " " << num5 << endl;
+
     return 0;
 }
-// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
-// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
-
-// 開始使用的提示: 
-//   1. 使用 [方案總管] 視窗，新增/管理檔案
-//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
-//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
-//   4. 使用 [錯誤清單] 視窗，檢視錯誤
-//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
-//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
