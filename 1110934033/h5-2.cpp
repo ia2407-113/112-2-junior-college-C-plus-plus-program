d9faeec21a9349b9f09189f23e1f82b4cbb96da8
ziv09 <163259868+ziv09@users.noreply.github.com>
2024-04-10 10:54:43 +0800

    Create h5-2.cpp

---
+++ 1110934033/h5-2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main() {
+    string id1, id2;
+    cout << "請輸入兩個身分證字號: ";
+    cin >> id1 >> id2;
+
+    char gender1 = id1[id1.length() - 9];
+    char gender2 = id2[id2.length() - 9];
+
+    if (gender1 == '1') {
+        cout << "第一位的身份證字號是男性" << endl;
+    } else {
+        cout << "第二位的身份證字號是女性" << endl;
+    }
+
+    if (gender2 == '2') {
+        cout << "第一位的身份證字號是女性" << endl;
+    } else {
+        cout << "第二位的身份證字號是男性" << endl;
+    }
+
+    string num1 = id1.substr(7, 9);
+    string num2 = id2.substr(7, 9);
+
+    if (num1 < num2) {
+        cout << "第一位的年齡較大" << endl;
+    } else if (num1 > num2) {
+        cout << "第二位的年齡較大" << endl;
+    } else {
+        cout << "兩位年齡一樣大" << endl;
+    }
+
+    return 0;
+}
