a0e195c859755b1f315ed6c8be6ec5fd3d3f89a4
ghyisjj55555 <109556902+ghyisjj55555@users.noreply.github.com>
2024-04-10 13:41:56 +0800

    0327

---
+++ 1111134020/非第一個英文字母改大寫/1111134020-1/1111134020-1.cpp
@@ -0,0 +1,42 @@
+﻿// 1111134020-1.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//翻開6-15頁
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+int main() {
+
+    string str1, str2;
+    cout << "輸入一串英文:";
+    getline(cin, str1);
+
+    for (int i = 0; i < str1.length(); i++) 
+    {
+        if (i == 0 || str1[i - 1] == ' ') 
+        {
+         str2 += tolower(str1[i]);
+        }
+        else
+        {
+            str2 += toupper(str1[i]);
+        }
+    }
+    
+    cout << str2;
+
+    return 0;
+}
+
+
+// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
+// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+
+// 開始使用的提示: 
+//   1. 使用 [方案總管] 視窗，新增/管理檔案
+//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
+//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
+//   4. 使用 [錯誤清單] 視窗，檢視錯誤
+//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
+//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
