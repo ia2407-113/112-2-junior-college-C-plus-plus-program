1c9d78f125d2e71d791b270b27ee3993c82c5318
cheseees <144889454+cheseees@users.noreply.github.com>
2024-04-10 13:19:36 +0800

    Add files via upload

---
+++ 1111134022/2024 022 327-1/2024 022 327-1/2024 022 327-1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int main() {
+    string str1;
+    int i = 0;
+    cout << "請輸入一句英文：";
+    getline(cin, str1);
+
+    do
+    {
+        if (isalnum(str1[i]))
+        {
+            if (i == 0 || str1[i - 1] == ' ') 
+            {
+            }
+            else
+            {
+                str1[i] = toupper(str1[i]);
+            }
+        }
+
+        i++;
+    } while (i < str1.length());
+
+    cout << str1;
+
+    return 0;
+}
+
