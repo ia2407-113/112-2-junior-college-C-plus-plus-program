d53fc6ee9ee81db9cf9d23fd7dd96d244008a8fc
Hsuan07531 <144888928+Hsuan07531@users.noreply.github.com>
2024-04-10 16:14:11 +0800

    Add files via upload

---
+++ 1111134042/0410/ConsoleApplication4.cpp
@@ -0,0 +1,64 @@
+﻿// ConsoleApplication4.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+#include "ConsoleApplication4.h"
+using namespace std;
+// 立方體體積計算函數
+void volume(int side, double&result)
+{
+    result = side * side * side;
+}
+
+// 球體體積計算函數
+void volume(double radius, double&result)
+{
+    result = (4.0 / 3.0) * 3.14159 * radius * radius * radius;
+}
+
+// 圓柱體體積計算函數
+void volume(double radius, double height, double&result)
+{
+    result = 3.14159 * radius * radius * height;
+}
+
+// 長方體體積計算函數
+void volume(double length, double width, double height, double&result) {
+    result = length * width * height;
+}
+
+int main() {
+    int side;
+    double radius, height, length, width;
+    double result;//儲存
+
+    // 立方體
+    cout << "請輸入立方體的邊長：";
+    cin >> side;
+    volume(side, result);//呼叫計算函式
+    cout << "立方體的體積為：" << result;
+
+    // 球體
+    cout << "請輸入球體的半徑：";
+    cin >> radius;
+    volume(radius, result);//呼叫計算函式
+    cout << "球體的體積為：" << result;
+
+    // 圓柱體
+    cout << "請輸入圓柱體的半徑：";
+    cin >> radius;
+    cout << "請輸入圓柱體的高度：";
+    cin >> height;
+    volume(radius, height, result);//呼叫計算函式
+    cout << "圓柱體的體積為：" << result;
+
+    // 長方體
+    cout << "請輸入長方體的長：";
+    cin >> length;
+    cout << "請輸入長方體的寬：";
+    cin >> width;
+    cout << "請輸入長方體的高：";
+    cin >> height;
+    volume(length, width, height, result);//呼叫計算函式
+    cout << "長方體的體積為：" << result;
+}
---
+++ 1111134042/0410/ConsoleApplication5.cpp
@@ -0,0 +1,53 @@
+﻿// ConsoleApplication5.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+using namespace std;
+// 自訂函式，將傳入的整數陣列按照從小到大的順序排序
+void num(int* arr, int size) 
+{
+    for (int i = 0; i < size - 1; ++i) 
+    {
+        for (int j = 0; j < size - i - 1; ++j) 
+        {
+            if (arr[j] > arr[j + 1]) 
+            {
+                // 交換兩個元素的值
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    const int size = 5;
+    int arr[size];
+
+    cout << "請輸入5個整數：";
+    for (int i = 0; i < size; ++i) {
+        cin >> arr[i];
+    }
+
+    // 使用自訂函式進行排序
+    num(arr, size);
+
+    // 輸出排序後的結果
+    cout << "排序後的整數為：";
+    for (int i = 0; i < size; ++i) {
+        cout << arr[i] << " ";
+    }
+}
+
+
+// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
+// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+
+// 開始使用的提示: 
+//   1. 使用 [方案總管] 視窗，新增/管理檔案
+//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
+//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
+//   4. 使用 [錯誤清單] 視窗，檢視錯誤
+//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
+//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
