833a15b4dee8debae8583525573b81d00058ffde
sillkjj <144888800+neil950908@users.noreply.github.com>
2024-04-24 15:53:12 +0800

    Add files via upload

---
+++ 1111134036/1111134036/App1/App1/App.xaml.h
@@ -0,0 +1,27 @@
+﻿//
+// App.xaml.h
+// App 類別的宣告。
+//
+
+#pragma once
+
+#include "App.g.h"
+
+namespace App1
+{
+	/// <summary>
+	/// 提供應用程式專屬行為以補充預設的應用程式類別。
+	/// </summary>
+	ref class App sealed
+	{
+	protected:
+		virtual void OnLaunched(Windows::ApplicationModel::Activation::LaunchActivatedEventArgs^ e) override;
+
+	internal:
+		App();
+
+	private:
+		void OnSuspending(Platform::Object^ sender, Windows::ApplicationModel::SuspendingEventArgs^ e);
+		void OnNavigationFailed(Platform::Object ^sender, Windows::UI::Xaml::Navigation::NavigationFailedEventArgs ^e);
+	};
+}
---
+++ 1111134036/1111134036/App1/App1/MainPage.xaml.h
@@ -0,0 +1,21 @@
+﻿//
+// MainPage.xaml.h
+// MainPage 類別的宣告。
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+
+namespace App1
+{
+	/// <summary>
+	/// 可以在本身使用或巡覽至框架內的空白頁面。
+	/// </summary>
+	public ref class MainPage sealed
+	{
+	public:
+		MainPage();
+
+	};
+}
