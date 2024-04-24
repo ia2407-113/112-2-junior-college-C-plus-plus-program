ef7d2d34de2ad920255c3aaa6e3bbc2ee607842a
tutjui7y6i <144888790+tutjui7y6i@users.noreply.github.com>
2024-04-24 15:53:17 +0800

    Add files via upload

--- 1111134033/1111134033/1111134033/1111134033.cpp
+++ 1111134033/1111134033/1111134033/1111134033.cpp
@@ -4,41 +4,61 @@
 #include <iostream>
 #include <string>
 using namespace std;
-void bubble(int arr[], int num);
+int main()
+{
+    int i, j;
+    struct student
+    {
+        string name;
+        int age;
+        string tel;
+        void printdata(void)
+        {
+            cout << name << '\t'
+                << age << '\t'
+                << tel << '\t' << endl;
+        }
+    };
+    student telephone[3], temp;
+    // 宣告指標變數ptr，並配置動態記憶體來儲存結構資料 
+    // 同時ptr指向動態記憶體的起始位址
+    struct student* ptr = new struct student;
+    for (i = 0; i <= 2; i++)
+    {
+        cout << "輸入學生的名字:";
+        cin >> (*ptr).name;
+        cout << "年齡:";
+        cin >> (*ptr).age;
+        cout << "電話:";
+        cin >> (*ptr).tel;
 
-int main() {
-    int size[5];
-
-    for (int i = 0; i < 5; i++) {
-        cout << "請輸入數字";
-        cin >> size[i];
-
+        cout << "學生的名字:" << (*ptr).name;
+        cout << "\t年齡:" << (*ptr).age;
+        cout << "\t電話:" << (*ptr).tel << endl;
+        telephone[i] = (*ptr);
     }
-    bubble(size, 5);
+    cout << "排序前的資料:" << endl;
+    for (i = 0; i < 3; i++)
+        telephone[i].printdata();
+
+    for (i = 1; i <= 2; i++)//執行2(=3-1)個步驟
+        for (j = 0; j < 3 - i; j++)//第i步驟,執行3-i次比較
+            if (telephone[j].age > telephone[j + 1].age)
+            {
+                temp = telephone[j];
+                telephone[j] = telephone[j + 1];
+                telephone[j + 1] = temp;
+            }
+    //若左邊的資料>右邊的資料，則
+    //將telephone[j]與telephone[j+1]的
+    //所有成員變數之內容互換。
+    cout << "排序後的資料:" << endl;
+
+    for (i = 0; i < 3; i++)
+        telephone[i].printdata();
+
     return 0;
 }
-
-void bubble(int arr[], int size)
-{
-    for (unsigned int pass = 1; pass < size; ++pass)
-    {
-        for (int i = 0; i < size - 1; ++i)
-        {
-            if (arr[i] > arr[i + 1])
-            {
-                int hold = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = hold;
-            }
-        }
-    }
-    cout << "\n資料項目排列後印出\n";
-    for (int g = 0; g < 5; g++)
-    {
-        cout << arr[g] << ' ';
-    }
-}
-
 // 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
 // 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
 
