8da5569687b0e2e989486d1ce24ed0c5c1875899
kyoki <135029618+kyoki618@users.noreply.github.com>
2024-04-24 15:52:10 +0800

    Add files via upload

---
+++ 1110834023/0424/ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+struct student {
+    string name;
+    int age;
+    string tel;
+};
+
+int main() {
+    int num_students = 3;
+    struct student* telephone = new struct student[num_students];
+
+    for (int i = 0; i < num_students; ++i) {
+        cout << "輸入第 " << i + 1 << " 位同學的名字:";
+        cin >> telephone[i].name;
+        cout << "年齡:";
+        cin >> telephone[i].age;
+        cout << "電話:";
+        cin >> telephone[i].tel;
+    }
+
+    for (int i = 0; i < num_students - 1; ++i) {
+        for (int j = 0; j < num_students - i - 1; ++j) {
+            if (telephone[j].tel > telephone[j + 1].tel) {
+                struct student temp = telephone[j];
+                telephone[j] = telephone[j + 1];
+                telephone[j + 1] = temp;
+            }
+        }
+    }
+
+
+    cout << "排序後的通訊錄:" << endl;
+    for (int i = 0; i < num_students; ++i) {
+        cout << "姓名:" << telephone[i].name << "\t年齡:" << telephone[i].age << "\t電話:" << telephone[i].tel << endl;
+    }
+
+    // 釋放ptr所指向的動態記憶體
+    delete[] telephone;
+
+    return 0;
+}
