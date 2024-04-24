a7fb96799bb2a3f3867ee918aca45975a81e0baf
張博鈞 <80932235+2003-David@users.noreply.github.com>
2024-04-24 16:01:34 +0800

    Add files via upload

---
+++ 1110831032/424-1/424-1.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+struct Student {
+    std::string name;
+    int age;
+    std::string phone;
+
+    void printData() {
+        std::cout << name << '\t' << age << '\t' << phone << std::endl;
+    }
+};
+
+bool compareByPhone(const Student& a, const Student& b) {
+    return a.phone < b.phone;
+}
+
+int main() {
+    const int numStudents = 3;
+    Student* students = new Student[numStudents];
+
+    // 輸入資料
+    for (int i = 0; i < numStudents; ++i) {
+        std::cout << "請輸入第" << i + 1 << "位同學的姓名 年齡 電話：";
+        std::cin >> students[i].name >> students[i].age >> students[i].phone;
+    }
+
+    // 根據電話號碼排序
+    std::sort(students, students + numStudents, compareByPhone);
+
+    // 輸出結果
+    std::cout << "依據電話號碼排序後的通訊錄：" << std::endl;
+    for (int i = 0; i < numStudents; ++i) {
+        students[i].printData();
+    }
+
+    // 釋放動態配置的記憶體
+    delete[] students;
+
+    return 0;
+}
