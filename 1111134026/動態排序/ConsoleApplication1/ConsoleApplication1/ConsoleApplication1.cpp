7f6689675a56464c0b2c4ee9cb1e84c33524ece4
Alann999 <144888962+Alann999@users.noreply.github.com>
2024-04-24 16:12:31 +0800

    20240424

---
+++ 1111134026/動態排序/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int i, j;
+    struct student
+    {
+        string name;
+        int age;
+        string tel;
+        void printdata(void)
+        {
+            cout << name <<'\t';
+            cout << age <<'\t';
+            cout << tel <<'\t' << endl;
+        }
+    };
+    student telephone[3],temp;
+    struct student* ptr = new struct student;
+    for (i = 0; i < 3; i++)
+    {
+        cout << "輸入學生的名字:";
+        cin >> (*ptr).name;
+        cout << "年齡:";
+        cin >> (*ptr).age;
+        cout << "電話:";
+        cin >> (*ptr).tel;
+
+        cout << "學生的名字:" << (*ptr).name;
+        cout << "\t年齡:" << (*ptr).age;
+        cout << "\t電話:" << (*ptr).tel << endl;
+        telephone[i] = *ptr;
+    }
+
+
+    cout << "排序前的資料:" << endl;
+    for (i = 0; i < 3; i++)
+        telephone[i].printdata();
+
+    for (i = 1; i <= 2; i++)
+        for (j = 0; j < 3 - i; j++)
+            if (telephone[j].age > telephone[j + 1].age)
+            {
+                temp = telephone[j];
+                telephone[j] = telephone[j + 1];
+                telephone[j + 1] = temp;
+            }
+
+    cout << "排序後的資料:" << endl;
+
+    for (i = 0; i < 3; i++)
+        telephone[i].printdata();
+
+    delete ptr;
+    ptr = 0;
+
+    return 0;
+}
