a8d657ae77b7cc46b45b6a1474e2b59c3b522d74
Qixuan0116 <144888878+Qixuan0116@users.noreply.github.com>
2024-05-01 16:00:14 +0800

    Add files via upload

---
+++ 1111134039/20240501/ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+
+// Fig. 3.1: fig03_01.cpp
+// Define class GradeBook with a member function displayMessage;
+// Create a GradeBook object and call its displayMessage function.
+using namespace std;
+
+// GradeBook class definition
+class GradeBook
+{
+public:
+    string a, b;
+    // function that displays a welcome message to the GradeBook user
+    void setCoursecoureID()
+    {
+        cout << "請輸入科目";
+        cin >> a;
+        courseID = a;
+    }
+
+    void setCourseTeacher()
+    {
+        cout << "請輸入老師";
+        cin >> b;
+        teacher = b;
+    }
+    string getCoursecoureID()
+    {
+        return courseID;
+    }
+    string getCourseTeacher()
+    {
+        return teacher;
+    }
+
+private:
+    string courseID;
+    string teacher;
+}; // end class GradeBook  
+
+// function main begins program execution
+int main()
+{
+    string c, d;
+    GradeBook myGradeBook;
+    myGradeBook.setCoursecoureID();
+    myGradeBook.setCourseTeacher();
+
+    c = myGradeBook.getCoursecoureID();
+    d = myGradeBook.getCourseTeacher();
+    cout << c <<"\n";
+    cout << d;
+} // end main
