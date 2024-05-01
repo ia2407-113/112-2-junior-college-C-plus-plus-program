33bbefaca903ff88a576dfebfcf79177fc181554
ADIJER <144888843+Saiaserd@users.noreply.github.com>
2024-05-01 16:08:29 +0800

    Add files via upload

---
+++ 1111134025/0501類別與物件/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class GradeBook
+{
+public:
+    void displayMessage() const{
+        cout << "id是" << courseID << endl;
+        cout << "老師是" << teacher << endl;
+    } 
+
+    void setCourseID(string id){
+        if (id != "")
+            courseID = id;
+    }
+
+    void setCourseTeacher(string teach) {
+        if (teach != "")
+            teacher=teach;
+    }
+
+    string getCourseID(){
+        return courseID;
+    }
+
+    string getCourseTeacher(){
+        return teacher;
+    }
+
+private:
+    string courseID = "114514";
+    string teacher = "Cupcakke";
+};
+
+int main()
+{
+    GradeBook myGradeBook;
+    myGradeBook.displayMessage();
+    myGradeBook.setCourseID("228922");
+    myGradeBook.setCourseTeacher("jiafei");
+    myGradeBook.displayMessage();
+    myGradeBook.getCourseID();
+    myGradeBook.getCourseTeacher();
+} 
