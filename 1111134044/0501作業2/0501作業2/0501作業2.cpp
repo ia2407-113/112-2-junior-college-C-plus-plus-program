5844dd3674ae7b76c7f01be1968ba1d92bd48bd3
Louis542466 <144888618+Louis542466@users.noreply.github.com>
2024-05-01 15:59:54 +0800

    0501作業

---
+++ 1111134044/0501作業2/0501作業2/0501作業2.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+
+using namespace std;
+class GradeBook
+{
+public:
+
+    void displayMessage() const
+    {
+        cout << "Welcome to the Grade Book!" << endl;
+        cout << "Course Name is: " << course_name << endl;
+        cout << "setCourseID is: " << courseID << endl;
+        cout << "setCourseTeacher is: " << teacher << endl;
+    }
+
+    void setCourse(string name)
+    {
+        if (name != "")
+            course_name = name;
+    }
+    void setCourseID(string a)
+    {
+        if (a != "")
+        {
+            courseID = a;
+        }
+
+    }
+    void setCourseTeacher(string b)
+    {
+        if (b != "")
+        {
+            teacher = b;
+        }
+    }
+    string getCourseID()
+    {
+        return courseID;
+    }
+    string getCourseTeacher()
+    {
+        return teacher;
+    }
+private:
+    string courseID = "hello";
+    string teacher = "world";
+    string course_name = "Computer Networks";
+};
+
+
+int main()
+{
+    GradeBook myGradeBook;
+    myGradeBook.displayMessage();
+    myGradeBook.setCourse("C++ programming");
+    myGradeBook.setCourseID("我誰!");
+    myGradeBook.setCourseTeacher("我瘋子!");
+    myGradeBook.displayMessage();
+    cout << myGradeBook.getCourseID();
+    cout << myGradeBook.getCourseTeacher();
+    
+}
