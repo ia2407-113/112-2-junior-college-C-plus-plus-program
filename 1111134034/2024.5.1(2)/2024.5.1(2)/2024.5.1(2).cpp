077275e139cca326c4b722f5657fd285d1c7d233
yihuang1022 <144888696+yihuang1022@users.noreply.github.com>
2024-05-01 16:00:08 +0800

    Add files via upload

---
+++ 1111134034/2024.5.1(2)/2024.5.1(2)/2024.5.1(2).cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+
+// Fig. 3.1: fig03_01.cpp
+// Define class GradeBook with a member function displayMessage;
+// Create a GradeBook object and call its displayMessage function.
+using namespace std;
+
+// GradeBook class definition
+class GradeBook
+{
+public:
+    string a,b;
+
+
+    void setCourseID()
+    {
+        cout << "請輸入科目:";
+        cin >> a;
+        courseID = a;
+    }
+    void setCourseTeacher()
+    {
+        cout << "請輸入老師:";
+        cin >> b;
+        teacher = b;
+    }
+    string getCourseID()
+    {
+        return courseID;
+    }
+    string getCourseTeacher()
+    {
+        return teacher;
+    }
+
+private:
+    string courseID;
+    string teacher;
+};
+
+// function main begins program execution
+int main()
+{
+    string c, d;
+    GradeBook myGradeBook;
+    myGradeBook.setCourseID();
+    myGradeBook.setCourseTeacher();
+
+    c = myGradeBook.getCourseID();
+    d = myGradeBook.getCourseTeacher();
+
+    cout <<"科目:"<< c << "\n";
+    cout <<"老師:"<< d;
+} // end main
