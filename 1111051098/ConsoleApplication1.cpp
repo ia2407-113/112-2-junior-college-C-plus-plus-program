29aaf6fd5a7bc31d47d82ebfcea39f47e33317c2
林江翰 <144888491+Granny1221@users.noreply.github.com>
2024-05-01 16:05:55 +0800

    5/1 物件導向

---
+++ 1111051098/ConsoleApplication1.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+
+// Fig. 3.1: fig03_01.cpp
+// Define class GradeBook with a member function displayMessage;
+// Create a GradeBook object and call its displayMessage function.
+using namespace std;
+
+// GradeBook class definition
+class GradeBook
+{
+public:
+    string Cname, Tname;
+    void setCourseID()
+    {
+        cout << "請輸入課程名稱";
+        cin >> Cname;
+        courseID = Cname;
+    }
+    void setCourseTeacher()
+    {
+        cout << "請輸入老師名稱";
+        cin >> Tname;
+        teacher = Tname;
+    }
+    string getCourse()
+    {
+        return courseID;
+    }
+    string getCourseTeacher()
+    {
+        return teacher;
+    }
+private:
+    string courseID;
+    string teacher;
+}; 
+int main()
+{
+    string cou,tea; 
+    GradeBook myGradeBook; 
+    myGradeBook.setCourseID();
+    myGradeBook.setCourseTeacher();
+
+    cou = myGradeBook.getCourse();
+    tea = myGradeBook.getCourseTeacher();
+
+    cout << "課程名稱" << cou << endl;
+    cout << "老師姓名是" << tea << endl;
+} 
