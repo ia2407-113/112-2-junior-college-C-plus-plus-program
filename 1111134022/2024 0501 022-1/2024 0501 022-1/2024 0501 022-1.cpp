fc963c706311206eeab9a41a93b20f511f7fbc6b
cheseees <144889454+cheseees@users.noreply.github.com>
2024-05-01 16:02:14 +0800

    Add files via upload

---
+++ 1111134022/2024 0501 022-1/2024 0501 022-1/2024 0501 022-1.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+
+using namespace std;
+
+
+class GradeBook
+{
+   
+public:
+    string a, b,c;
+    void setCourseID()
+    {
+        cout << "請輸入科目"  ;
+        cin >> a;
+        CourseID=a;
+    }
+
+    void setCourseTeacher()
+    {
+        cout << "請輸入老師";
+        cin >> b;
+        teacher = b;
+    }
+    string getCourseID()
+    {
+        return  CourseID;
+    }
+    string getCourseTeacher()
+    {
+        return teacher;
+    }
+ 
+private:
+    string CourseID;
+    string teacher;
+};
+int main()
+{
+    string c, d;
+    GradeBook mygradeBook;
+    mygradeBook.setCourseID();
+    mygradeBook.setCourseTeacher();
+    c = mygradeBook.getCourseID();
+    d = mygradeBook.getCourseTeacher();
+    cout << "課程為" << c << "\n";
+    cout << "老師為" << d << "\n";
+}
