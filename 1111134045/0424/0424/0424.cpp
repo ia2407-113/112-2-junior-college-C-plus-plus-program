da8bd6e6f963e16f44dfc14c8894f8a68b84f3ae
Zehong960806 <144889047+Zehong960806@users.noreply.github.com>
2024-05-01 15:55:01 +0800

    Add files via upload

---
+++ 1111134045/0424/0424/0424.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int i, j;
+    struct tel_book
+    {
+        string name;
+        int age;
+        string tel;
+
+        void printdata(void)
+        {
+            cout << name << '\t'
+                << age << '\t'
+                << tel << '\t' << endl;
+        }
+    };
+    struct tel_book* input = new struct tel_book;
+    tel_book tellphone[3];
+    for (int i = 0; i < 2; i++)
+    {
+        cout << "輸入姓名:";
+        cin >> (*input).name;
+        cout << "輸入年齡:";
+        cin >> (*input).age;
+        cout << "輸入電話:";
+        cin >> (*input).tel;
+        cout << "<學生姓名為" << (*input).name;
+        cout << ",學生年齡為" << (*input).age;
+        cout << ",學生電話為" << (*input).tel << ">\n";
+        tellphone[i] = *input;
+    }
+    tel_book temp;
+
+    for (i = 1; i <= 2; i++)//執行2(=3-1)個步驟
+        for (j = 0; j < 3 - i; j++)//第i步驟,執行3-i次比較
+            if (tellphone[j].age > tellphone[j + 1].age)
+            {
+                temp = tellphone[j];
+                tellphone[j] = tellphone[j + 1];
+                tellphone[j + 1] = temp;
+            }
+
+    cout << "排序後的資料:\n";
+
+    for (i = 1; i < 3; i++)
+        tellphone[i].printdata();
+    delete[] input;
+    input = NULL;
+    return 0;
+}
