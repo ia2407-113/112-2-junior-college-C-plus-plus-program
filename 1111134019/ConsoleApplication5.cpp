0a1d251a4bb7c908771ff7be8dadaae956bad6a4
xuan0707 <144889037+xuan0707@users.noreply.github.com>
2024-05-01 15:49:13 +0800

    Add files via upload

--- 1111134019/ConsoleApplication5.cpp
+++ 1111134019/ConsoleApplication5.cpp
@@ -2,15 +2,65 @@
 //
 
 #include <iostream>
+#include <string> // 包含 string 標頭以使用字串資料型別
 
 using namespace std;
+
+// GradeBook 類別定義
+class GradeBook
+{
+public:
+    // 函數用於向使用者顯示歡迎訊息
+    void displayMessage() const
+    {
+        cout << "歡迎使用成績登錄系統！" << endl;
+        cout << "課程名稱：" << course_name << endl;
+        cout << "課程編號：" << courseID << endl;
+        cout << "授課教師：" << teacher << endl;
+    }
+
+    // 函數用於設定課程名稱
+    void setCourse(string name)
+    {
+        if (!name.empty())
+            course_name = name;
+    }
+
+    // 函數用於設定課程編號
+    void setCourseID(string id)
+    {
+        if (!id.empty())
+            courseID = id;
+    }
+
+    // 函數用於設定授課教師
+    void setCourseTeacher(string t)
+    {
+        if (!t.empty())
+            teacher = t;
+    }
+
+private:
+    string course_name = "計算機概論"; // 預設課程名稱
+    string courseID = "34019"; // 預設課程編號
+    string teacher = "G"; // 預設教師名稱
+};
+
+// 主函數開始程式執行
 int main()
 {
-	int d;
-	cout << ("輸入購物金額:");
-	cin >> d;
-	cout << "禮券金額為" << d/10000*1000 << "元";
-	return 0;
+    GradeBook myGradeBook; // 創建一個名為 myGradeBook 的 GradeBook 物件
+    myGradeBook.displayMessage(); // 呼叫物件的 displayMessage 函數
+
+    // 設定課程資訊
+    myGradeBook.setCourse("C++ 程式設計");
+    myGradeBook.setCourseID("340190");
+    myGradeBook.setCourseTeacher("G");
+
+    // 顯示更新後的課程資訊
+    myGradeBook.displayMessage();
+
+    return 0;
 }
 // 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
 // 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
