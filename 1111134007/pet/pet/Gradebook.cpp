63014c69335ce6039c73b21c99b922a427028b95
1111134007tingting <144888945+1111134007tingting@users.noreply.github.com>
2024-05-08 15:51:24 +0800

    0508

---
+++ 1111134007/pet/pet/Gradebook.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "Gradebook.h"
+using namespace std;
+
+GradeBook:: GradeBook(string initial_name)
+{
+    course_name = initial_name;
+}
+
+GradeBook::~GradeBook()
+{
+    cout << "Bye Bye my course!" << endl;
+}
+
+void GradeBook::displayMessage() const
+{
+    cout << "Welcome to the Grade Book!" << endl;
+    cout << "Course Name is: " << course_name << endl;
+} // end function displayMessage
+
+void GradeBook::setCourse(string name)
+{
+    if (name != "")
+        course_name = name;
+}
+string GradeBook::getcourse()
+{
+    return course_name;
+}
---
+++ 1111134007/pet/pet/pet.cpp
@@ -0,0 +1,14 @@
+﻿// pet.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+#include "Gradebook.h"
+using namespace std;
+int main()
+{
+    GradeBook myGradeBook("Course name is unkown."); // create a GradeBook object named myGradeBook
+    myGradeBook.displayMessage(); // call object's displayMessage function 
+    myGradeBook.setCourse("C++ programming");
+    //myGradeBook.course_name = "I don't know!";
+    myGradeBook.displayMessage();
+} // end main
