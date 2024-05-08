90571fe8b493fcdaaca0e2cef500b2ab1b1ef7bd
林江翰 <144888491+Granny1221@users.noreply.github.com>
2024-05-08 15:50:17 +0800

    修改以上程式碼，加入建構子與解構子，建構子提供初始化的課程名稱，解構子則負責印出結束的訊息。

---
+++ 1111051098/ConsoleApplication4.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+class GradeBook
+{
+public:
+     GradeBook(string initial_name)
+    {
+         course_name = initial_name;
+    }
+     ~GradeBook()
+     {
+         cout << "Bye Bye course";
+     }
+    void displayMessage() const
+    {
+        cout << "Welcome to the Grade Book!" << endl;
+        cout << "Course Name is: " << course_name << endl;
+    } 
+
+    void setCourse(string name)
+    {
+        if (name != "")
+            course_name = name;
+    }
+private:
+    string course_name = "Computer Networks";
+};
+int main()
+{
+    GradeBook myGradeBook("English");
+    myGradeBook.displayMessage();
+    myGradeBook.setCourse("C++ programming");
+    myGradeBook.displayMessage();
+} 
