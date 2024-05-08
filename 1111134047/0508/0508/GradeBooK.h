91ad44cbb4482617d3e176f9a52ac2251f834148
chengyu0116 <144889049+chengyu0116@users.noreply.github.com>
2024-05-08 16:57:08 +0800

    Add files via upload

---
+++ 1111134047/0508/0508/GradeBooK.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class GradeBook
+{
+public:
+    GradeBook(string a_course);
+
+    ~GradeBook();
+
+    void displayMessage() const;
+
+    void setCourse(string name);
+private:
+    string course_name;
+};
