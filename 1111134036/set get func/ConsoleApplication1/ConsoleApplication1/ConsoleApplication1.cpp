5d64843acbc7a14f32189395e2108936d08028bc
sillkjj <144888800+neil950908@users.noreply.github.com>
2024-05-10 13:27:45 +0800

    Add files via upload

---
+++ 1111134036/set get func/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+using namespace std;
+class GradeBook
+{
+public:
+    void displayMessage() const
+    {
+        cout << "Welcome to the Grade Book!" << endl;
+        cout << "Course Name is: " << course_name << endl;
+    } 
+
+    void setCourse(string name)
+    {
+        if (name != "")
+            course_name = name;
+    }
+    void setCourseID(string ID)
+    {
+        if (ID != "")
+            courseID = ID;
+    }
+    void setCourseTeacher(string inputteacher)
+    {
+        teacher = inputteacher;
+    }
+    string getCourseID()
+    {
+        return courseID;
+    }
+    string getCourseTeacher()
+    {
+        return teacher;
+    }
+private:
+    string course_name = "Computer Networks";
+    string courseID = "123";
+    string teacher = "abc";
+}; 
+int main()
+{
+    GradeBook myGradeBook; 
+    myGradeBook.displayMessage(); 
+    myGradeBook.setCourse("C++ programming");
+    myGradeBook.displayMessage();
+    myGradeBook.setCourseID("456");
+    myGradeBook.setCourseTeacher("def");
+    cout << myGradeBook.getCourseID() << endl;
+    cout << myGradeBook.getCourseTeacher() << endl;
+} 
