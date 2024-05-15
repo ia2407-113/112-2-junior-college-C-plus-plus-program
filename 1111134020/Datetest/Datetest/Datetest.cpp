32304e602b18753cb68ea2c973fe24b29b15e48e
ghyisjj55555 <109556902+ghyisjj55555@users.noreply.github.com>
2024-05-15 15:54:06 +0800

    0515

---
+++ 1111134020/Datetest/Datetest/Datetest.cpp
@@ -0,0 +1,30 @@
+﻿// Datetest.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+#include "date.h" // Employee class definition
+#include "pet.h"
+using namespace std;
+
+int main()
+{
+	Date birth(7, 24, 1949),birth1(2,11,1968), birth2(1,1,1990);
+	Date adopt(3, 12, 1988), adopt1(8,30,2000), adopt2(5, 14, 2019);
+	pet manager("pp", "短毛貓", birth, adopt);
+	pet manager1("gg", "米克斯貓", birth1, adopt1);
+	pet manager2("小黃", "狗", birth2, adopt2);
+	//manager.print();
+	cout << endl;
+	
+} // end main
+
+// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
+// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+
+// 開始使用的提示: 
+//   1. 使用 [方案總管] 視窗，新增/管理檔案
+//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
+//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
+//   4. 使用 [錯誤清單] 視窗，檢視錯誤
+//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
+//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
