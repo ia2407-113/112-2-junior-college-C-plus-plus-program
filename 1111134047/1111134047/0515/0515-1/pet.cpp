7038e3094c3ea8ac855784822a377e82772ec062
chengyu0116 <144889049+chengyu0116@users.noreply.github.com>
2024-05-15 16:49:47 +0800

    Add files via upload

---
+++ 1111134047/1111134047/0515/0515-1/pet.cpp
@@ -0,0 +1,40 @@
+// Fig. 9.20: Employee.cpp
+// Employee class member-function definitions.
+#include <iostream>
+#include "pet.h" // Employee class definition
+#include "Date.h" // Date class definition
+using namespace std;
+
+// constructor uses member initializer list to pass initializer 
+// values to constructors of member objects  
+Pet::Pet(const string& first, const string& last,
+    const Date& dateOfBirth, const Date& dateOfAdopt)
+    : firstName(first), // initialize firstName
+    lastName(last), // initialize lastName
+    birthDate(dateOfBirth), // initialize birthDate
+    hireDate(dateOfAdopt) // initialize hireDate
+{
+    // output Employee object to show when constructor is called
+    cout << "Pet object constructor: "
+        << firstName << ' ' << lastName << endl;
+} // end Employee constructor
+
+// print Employee object
+void Pet::print() const
+{
+    cout << lastName << ", " << firstName << " Adopt: ";
+    hireDate.print();
+    hireDate.printROC();
+    cout << "  Birthday: ";
+    birthDate.print();
+    birthDate.printROC();
+    
+    cout << endl;
+} // end function print
+
+// output Employee object to show when its destructor is called
+Pet::~Pet()
+{
+    cout << "Pet object destructor: "
+        << lastName << ", " << firstName << endl;
+} // end ~Employee destructor
