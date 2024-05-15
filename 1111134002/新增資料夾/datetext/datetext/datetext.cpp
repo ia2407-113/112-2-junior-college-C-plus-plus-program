a95599cbb94b67a40aae6770a2a1a5ca27a189d5
Zttt30 <144888765+Zttt30@users.noreply.github.com>
2024-05-15 15:50:55 +0800

    Add files via upload
    0515 date test

---
+++ 1111134002/新增資料夾/datetext/datetext/datetext.cpp
@@ -0,0 +1,18 @@
+﻿// Fig. 9.21: fig9_21.cpp
+// Demonstrating composition--an object with member objects.
+#include <iostream>
+#include "Date.h" // Employee class definition
+#include "pet.h"
+using namespace std;
+
+int main()
+{
+	Date birth(7, 24, 1949); Date birth1(5, 19, 1964);
+	Date adopt1(3, 12, 1958); Date adopt2(3, 12, 1972);
+	Date birth3(3, 12, 1958); Date adopt3(3, 12, 1972);
+	Pet parroe1("Luffy", "monkey", birth, adopt1);
+	Pet parroe2("Chopper", "馴鹿", birth1, adopt2);
+	Pet parroe3("Sanji", "dog", birth3, adopt3);
+	cout << endl;
+	//manager.print();
+} // end main
---
+++ 1111134002/新增資料夾/datetext/datetext/pet.cpp
@@ -0,0 +1,40 @@
+// Fig. 9.20: Employee.cpp
+// Employee class member-function definitions.
+#include <iostream>
+#include "pet.h" // Employee class definition
+#include "Date.h" // Date class definition
+using namespace std;
+
+// constructor uses member initializer list to pass initializer 
+// values to constructors of member objects  
+Pet::Pet(const string& first, const string& last,
+    const Date& dateOfBirth, const Date& dateOfHire)
+    : firstName(first), // initialize firstName
+    lastName(last), // initialize lastName
+    birthDate(dateOfBirth), // initialize birthDate
+    adoptDate(dateOfHire) // initialize hireDate
+{
+    // output Employee object to show when constructor is called
+    cout <<"\n" << "Pet object constructor: "
+        << firstName << ' ' << lastName << endl;
+    print();
+} // end Employee constructor
+
+// print Employee object
+void Pet::print() const
+{
+    cout << lastName << ", " << firstName << "\n" << "  adopt: ";
+    adoptDate.print();
+    adoptDate.printROC();
+    cout << "  Birthday: ";
+    birthDate.print();
+    birthDate.printROC();
+    cout << endl;
+} // end function print
+
+// output Employee object to show when its destructor is called
+Pet::~Pet()
+{
+    cout << "Pet object destructor: "
+        << lastName << ", " << firstName << endl;
+} // end ~Employee destructor
