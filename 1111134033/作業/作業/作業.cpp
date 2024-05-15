5219894bdf6154d9da441542999a197219e7b87d
tutjui7y6i <144888790+tutjui7y6i@users.noreply.github.com>
2024-05-15 16:04:12 +0800

    Add files via upload

---
+++ 1111134033/作業/作業/作業.cpp
@@ -0,0 +1,27 @@
+﻿// Fig. 9.21: fig9_21.cpp
+// Demonstrating composition--an object with member objects.
+#include <iostream>
+#include "pet.h" // Employee class definition
+using namespace std;
+
+int main()
+{
+	Date birth(7, 24, 1949), birth1(6, 13, 1955);
+	Date hire(3, 12, 1988), hire1(8, 11, 1991);
+	pet manager("Bob", "Blue", birth, hire);
+	pet manager1("Bob", "Blue", birth, hire);
+	cout << endl;
+	manager.print();
+	manager1.print();
+} // end main
+
+// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
+// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+
+// 開始使用的提示: 
+//   1. 使用 [方案總管] 視窗，新增/管理檔案
+//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
+//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
+//   4. 使用 [錯誤清單] 視窗，檢視錯誤
+//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
+//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
