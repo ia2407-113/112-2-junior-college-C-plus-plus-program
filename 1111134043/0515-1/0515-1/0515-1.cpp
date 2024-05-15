f2bd7281d66f81f68fa54dec220807e57fda50c6
jrfu0908 <144890122+jrfu0908@users.noreply.github.com>
2024-05-15 16:15:51 +0800

    Add files via upload

---
+++ 1111134043/0515-1/0515-1/0515-1.cpp
@@ -0,0 +1,30 @@
+﻿// 0515-1.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+#include "Date.h" // Employee class definition
+#include "pet.h"
+using namespace std;
+
+int main()
+{
+	Date birth(8,11,2020), birth1(2,18, 2020);
+	Date adopt(8, 11, 2020), adopt1(2,18, 1988);
+	Pet CAT("咪咪", "貓", birth, adopt);
+	Pet Hunter("美汁汁", "GH", birth1,adopt1);
+
+	cout << endl;
+	CAT.print();
+	Hunter.print();
+} // end main
+
+// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
+// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+
+// 開始使用的提示: 
+//   1. 使用 [方案總管] 視窗，新增/管理檔案
+//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
+//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
+//   4. 使用 [錯誤清單] 視窗，檢視錯誤
+//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
+//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
