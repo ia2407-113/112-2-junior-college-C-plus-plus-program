a7d7b85d8b61e60649f6241c9f17c43226168475
ilikechihuahua <144888566+ilikechihuahua@users.noreply.github.com>
2024-05-15 15:42:47 +0800

    Add files via upload

---
+++ 1111134018/ConsoleApplication4/ConsoleApplication4/pet.cpp
@@ -0,0 +1,54 @@
+// Fig. 9.20: Employee.cpp
+// Employee class member-function definitions.
+#include <iostream>
+#include "pet.h" // Employee class definition
+#include "Date.h" // Date class definition
+using namespace std;
+
+// constructor uses member initializer list to pass initializer 
+// values to constructors of member objects  
+pet::pet(const string& first, const string& last,
+    const Date& dateOfBirth, const Date& dateOfHire)
+    : firstName(first), // initialize firstName
+    lastName(last), // initialize lastName
+    birthDate(dateOfBirth), // initialize birthDate
+    hireDate(dateOfHire) // initialize hireDate
+{
+    // output Employee object to show when constructor is called
+    cout << "pet object constructor: "
+        << firstName << ' ' << lastName << endl;
+    print();
+} // end Employee constructor
+
+// print Employee object
+void pet::print() const
+{
+    cout << lastName << ", " << firstName << "  Adopt: ";
+    hireDate.print();
+    cout << "  Birthday: ";
+    birthDate.print();
+    birthDate.printROC();
+    cout << endl;
+} // end function print
+
+// output Employee object to show when its destructor is called
+pet::~pet()
+{
+    cout << "pet object destructor: "
+        << lastName << ", " << firstName << endl;
+} // end ~Employee destructor
+
+/**************************************************************************
+ * (C) Copyright 1992-2012 by Deitel & Associates, Inc. and               *
+ * Pearson Education, Inc. All Rights Reserved.                           *
+ *                                                                        *
+ * DISCLAIMER: The authors and publisher of this book have used their     *
+ * best efforts in preparing the book. These efforts include the          *
+ * development, research, and testing of the theories and programs        *
+ * to determine their effectiveness. The authors and publisher make       *
+ * no warranty of any kind, expressed or implied, with regard to these    *
+ * programs or to the documentation contained in these books. The authors *
+ * and publisher shall not be liable in any event for incidental or       *
+ * consequential damages in connection with, or arising out of, the       *
+ * furnishing, performance, or use of these programs.                     *
+ **************************************************************************/
