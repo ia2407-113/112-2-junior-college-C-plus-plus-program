a5386a81ef5259f29e0a6f131a849bb9785c09d6
ccy0118 <144888816+ccy0118@users.noreply.github.com>
2024-05-22 16:15:32 +0800

    05.22-3

---
+++ 1111134001/3/3/3.h
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+class Matrix
+{
+public:
+    int row, column;
+    int** element;
+    Matrix(int m, int n);
+    Matrix(char name, int m, int n);
+    Matrix operator^(Matrix x);
+};
