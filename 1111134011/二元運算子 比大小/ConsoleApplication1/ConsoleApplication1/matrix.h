6684f59bb5edfed5a48ecab50cd388d17c7a23cf
07158yunxin <144889045+07158yunxin@users.noreply.github.com>
2024-05-22 16:32:25 +0800

    20240522

---
+++ 1111134011/二元運算子 比大小/ConsoleApplication1/ConsoleApplication1/matrix.h
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+class Matrix
+{
+    public:
+    int row, column;
+    int** element;
+    Matrix(int m, int n);
+    
+
+    Matrix(char name, int m, int n);
+   
+
+    // 定義矩陣的「+」(加號)運算子的多載
+    Matrix operator^(Matrix x);
+    
+};
