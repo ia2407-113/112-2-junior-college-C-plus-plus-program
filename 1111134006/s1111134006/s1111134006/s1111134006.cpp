b95d3ef78995035e7c91dc07581827657a7db99c
1111134006 <144889442+1111134006@users.noreply.github.com>
2024-05-22 16:03:05 +0800

    0515

--- 1111134006/s1111134006/s1111134006/s1111134006.cpp
+++ 1111134006/s1111134006/s1111134006/s1111134006.cpp
@@ -1,46 +1,30 @@
 ﻿// s1111134006.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
 //
+
 #include <iostream>
+#include "date.h"
+#include "pet.h" 
+#include "Time.h" 
 using namespace std;
 
-// GradeBook class definition
-class GradeBook
-{
-public:
-    // function that displays a welcome message to the GradeBook user
-    void setCourseID(string ID)
-    {
-        if (ID != "")
-            courseID = ID;
-        cout << "courseID is: " << courseID << endl;
-    } // end function displayMessage
-    string getCourseID() {
-        return courseID;
-    };
-
-    void setCourseTeacher(string tea)
-    {
-        if (tea != "")
-            teacher = tea;
-        cout << "teacher is: " << teacher << endl;
-    };
-    string getCourseTeacher() {
-        return teacher;
-    }
-
-private:
-    string courseID = "2222";
-    string teacher = "banana";
-}; // end class GradeBook  
-
-// function main begins program execution
 int main()
 {
-    GradeBook myGradeBook; // create a GradeBook object named myGradeBook
-    myGradeBook.setCourseID("1111"); // call object's displayMessage function 
-    myGradeBook.setCourseTeacher("apple");
-    cout << myGradeBook.getCourseID() << endl;
-    cout << myGradeBook.getCourseTeacher() << endl;
-    //myGradeBook.course_name = "I don't know!";
+	Date birth(7, 24, 1949), birth1(5, 9, 1974);
+	Date adopt1(3, 12, 1988), adopt2(5, 9, 1974);
+	Time feed1(4, 30, 00), feed2(12, 00, 00);
+	Pet parrot1("肥肥", "非洲灰鸚鵡", birth, adopt1, feed1),
+		parrot2("壯壯", "亞馬遜鸚鵡", birth1, adopt2, feed2);
 
-} // end main
+	cout << endl;
+
+}
+// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
+// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+
+// 開始使用的提示: 
+//   1. 使用 [方案總管] 視窗，新增/管理檔案
+//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
+//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
+//   4. 使用 [錯誤清單] 視窗，檢視錯誤
+//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
+//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
