092b16e9cae321aede4555831da400882a590e1c
ccy0118 <144888816+ccy0118@users.noreply.github.com>
2024-05-22 16:19:53 +0800

    05.15-作業

---
+++ 1111134001/第十三週作業/DateTest/DateTest/DateTest.cpp
@@ -0,0 +1,17 @@
+﻿// Fig. 9.21: fig9_21.cpp
+// Demonstrating composition--an object with member objects.
+#include <iostream>
+#include "Date.h"
+#include "pet.h" // Employee class definition
+using namespace std;
+
+int main()
+{
+	Date birth(7, 24, 1949),birth1(5,9,1974);
+	Date adopt(3, 12, 1988),adopt1(1,24,1997);
+	Pet manager1("小白", "土狗", birth, adopt);
+	manager1.print();
+	Pet manager2("小黑", "拉不拉多", birth1, adopt1);
+	manager2.print();
+	cout << endl;
+} // end main
---
+++ 1111134001/第十三週作業/Pet_homework/Pet_homework/Time.h
@@ -0,0 +1,22 @@
+// Fig. 9.1: Time.h
+// Time class definition.
+// Member functions are defined in Time.cpp
+
+// prevent multiple inclusions of header
+#ifndef Time_H
+#define Time_H
+
+// Time class definition
+class Time
+{
+public:
+	Time(int h, int min, int s);
+	void printtime() const;	
+	~Time();
+private:
+	unsigned int hour; // 0 - 23 (24-hour clock format)
+	unsigned int minute; // 0 - 59
+	unsigned int second; // 0 - 59
+}; // end class Time
+
+#endif#pragma once
