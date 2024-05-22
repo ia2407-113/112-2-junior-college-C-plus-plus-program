9b90ca7d918bbc771ed2d0f247f1852d137a95a7
Zehong960806 <144889047+Zehong960806@users.noreply.github.com>
2024-05-22 16:11:35 +0800

    Add files via upload

---
+++ 1111134045/0522/0522/Matrix.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+using namespace std;
+class Matrix
+{
+public:
+    int row, column;
+    int** element;
+    Matrix(int m, int n);
+        // 定義矩陣的「++」(遞增)後置運算子的多載
+    Matrix operator++(int);
+};
