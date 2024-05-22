9e39d2930a8450adb3e626b151d210b587d65f47
07158yunxin <144889045+07158yunxin@users.noreply.github.com>
2024-05-22 16:36:06 +0800

    作業20240515

---
+++ 1111134011/pet time 類別/ConsoleApplication4/Time.cpp
@@ -0,0 +1,43 @@
+// print Time in universal-time format (HH:MM:SS)
+#include <iostream>
+#include <iomanip>
+#include <stdexcept> // for illegal_argument exception class
+#include "Time.h"
+
+using namespace std;
+
+Time::Time(int h, int min, int s)
+{
+    if (h >= 0 && h < 24)
+        hour = h;
+    else
+        throw invalid_argument("hour must be 0-23");
+
+    if (min >= 0 && min < 60)
+        minute = min;
+    else
+        throw invalid_argument("minute must be 0-59");
+
+    if (s >= 0 && s < 60)
+        second = s;
+    else
+        throw invalid_argument("second must be 0-59");
+
+    cout << "Time object constructor for time ";
+    printtime();
+    //printtime();
+    // output Date object to show when its constructor is called
+} // end Date constructor
+
+void Time::printtime() const
+{
+    cout << setfill('0') << setw(2) << hour << ":"
+        << setw(2) << minute << ":" << setw(2) << second << endl;
+} // end function printUniversal
+// print Time in standard-time format (HH:MM:SS AM or PM)
+
+Time::~Time()
+{
+    cout << "Time object destructor for time" << setfill('0') << setw(2) << hour << ":"
+        << setw(2) << minute << ":" << setw(2) << second << endl;
+}
---
+++ 1111134011/pet time 類別/ConsoleApplication4/pet.cpp
@@ -0,0 +1,47 @@
+//Pet.cpp  Pet class member-function definitions.
+#include <iostream>
+#include "pet.h" // Employee class definition
+#include "Date.h" // Date class definition
+#include "Time.h"
+using namespace std;
+
+// constructor uses member initializer list to pass initializer 
+// values to constructors of member objects  
+Pet::Pet(const string& first, const string& last,
+    const Date& dateOfBirth, const Date& dateOfHire,
+    const Time& timeOfmorning, const Time& timeOfnight)
+    : firstName(first), // initialize firstName
+    lastName(last), // initialize lastName
+    birthDate(dateOfBirth), // initialize birthDate
+    hireDate(dateOfHire), // initialize hireDate
+    morningFeed(timeOfmorning),
+    nightFeed(timeOfnight)
+{
+    // output Employee object to show when constructor is called
+    cout << "\nPet object constructor: "
+        << firstName << ' ' << lastName << endl;
+    print();
+} // end Employee constructor
+
+// print Employee object
+void Pet::print() const
+{
+    cout << lastName << ", " << firstName << endl << "Adopt: ";
+    hireDate.print();
+    hireDate.printROC();
+    cout << endl << "Birthday: ";
+    birthDate.print();
+    birthDate.printROC();
+    cout << endl << "Feeding time(morning): ";
+    morningFeed.printtime();
+    cout << "Feeding time(night): ";
+    nightFeed.printtime();
+    cout << endl;
+} // end function print
+
+// output Employee object to show when its destructor is called
+Pet::~Pet()
+{
+    cout << "Date object destructor for date"
+        << lastName << ", " << firstName << endl;
+} // end ~Employee destructor
