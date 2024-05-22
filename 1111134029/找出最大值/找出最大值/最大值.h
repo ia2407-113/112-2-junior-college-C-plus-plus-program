8f603d19cf59bccb2ce5cba0e39ab1de4664c79d
weegost <144888326+weegost@users.noreply.github.com>
2024-05-22 16:26:07 +0800

    Add files via upload

---
+++ 1111134029/找出最大值/找出最大值/最大值.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+using namespace std;
+class Matrix
+{
+public:
+    int row, column;
+    int** element;
+    Matrix(int m, int n);
+  
+
+    Matrix(char name, int m, int n);
+   
+
+    // 定義矩陣的「+」(加號)運算子的多載
+    Matrix operator^(Matrix x);
+
+};
