00876954d46c150c3ebc0bf485c9c0e0390f631e
tutjui7y6i <144888790+tutjui7y6i@users.noreply.github.com>
2024-05-22 16:02:02 +0800

    Add files via upload

---
+++ 1111134033/前置一元運算子多載/Matrix_test.cpp/Matrix_test.cpp.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include "matrix.h"
+using namespace std;
+
+int main()
+{
+    int i, j, row, column;
+    cout << "重新定義「-」前置運算子的多載，求A矩陣的副矩陣(-A)：\n";
+    cout << "輸入矩陣A的列數(row):";
+    cin >> row;
+    cout << "輸入矩陣A的行數(column):";
+    cin >> column;
+    Matrix a(row, column), b(row, column);
+    cout << "輸入一" << row << 'x' << column << "矩陣A" << endl;
+    for (i = 0; i < row; i++)
+        for (j = 0; j < column; j++)
+        {
+            cout << "A[" << i << "][" << j << "]=";
+            cin >> a.element[i][j];
+        }
+    b = -a;
+    cout << "矩陣A的負矩陣(-A):\n";
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < column; j++)
+            cout << b.element[i][j] << '\t';
+        cout << endl;
+    }
+
+    return 0;
+}
