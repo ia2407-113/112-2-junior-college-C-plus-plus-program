f8939fa2681147993b748b8291c881b4f63beeed
1111134007tingting <144888945+1111134007tingting@users.noreply.github.com>
2024-05-28 22:02:26 +0800

    HW3 (a,b矩陣內容找出最大值) 5/28

---
+++ 1111134007/Matrix3_overload (a,b個別找出最大值)/Matrix3_overload/matrix.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+#include"matrix.h"
+using namespace std;
+class Matrix
+{
+public:
+    int row, column;
+    int** element;
+    Matrix(int m, int n);
+
+
+        Matrix(char name, int m, int n);
+   
+  
+
+    // 定義矩陣的「+」(加號)運算子的多載
+    Matrix operator^(Matrix x);
+   
+};
