f85fd6d7eba316fe95cc79595faf67186a33a12d
1111134007tingting <144888945+1111134007tingting@users.noreply.github.com>
2024-05-28 22:00:06 +0800

    HW1 平方根 5/28

---
+++ 1111134007/Matrix_test 平方根/Matrix_test/matrix.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include"matrix.h"
+using namespace std ;
+      
+     Matrix:: Matrix(int m, int n)
+     {
+       row=m ;
+       column=n ;
+      
+       // 配置mxn的二維陣列element之動態記憶體
+       element = new int *[m];
+       int i, j;
+       for (i=0 ; i<m ; i++)
+          element[i] = new int [n];
+     }   
+	        
+    
+    // 定义矩阵的「^」(平方根)后置运算符的重载
+Matrix Matrix::operator^(int) {
+    Matrix temp(row, column); // 备份后置^运算符执行前的对象内容
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < column; j++) {
+            temp.element[i][j] = this->element[i][j];
+            this->element[i][j] = sqrt(this->element[i][j]);
+        }
+    }
+    return temp; // 返回后置^运算符执行前的对象内容
+}
+
+	
