567c68d07e70875526bef61638495e31565b5ca7
1111134007tingting <144888945+1111134007tingting@users.noreply.github.com>
2024-05-28 21:58:11 +0800

    HW 寵物餵食時間 5/15

---
+++ 1111134007/1111134007_0515/1111134007_0515/Pet.cpp
@@ -0,0 +1,44 @@
+//Pet.cpp  Pet class member-function definitions.
+#include <iostream>
+#include "Pet.h" // Employee class definition
+#include "Date.h" // Date class definition
+#include "Time.h"
+using namespace std;
+
+// constructor uses member initializer list to pass initializer 
+// values to constructors of member objects  
+Pet::Pet(const string& first, const string& last,
+    const Date& dateOfBirth, const Date& dateOfAdopt,
+    const Time& timeOfFeed)
+    : firstName(first), // initialize firstName
+    lastName(last), // initialize lastName
+    birthDate(dateOfBirth), // initialize birthDate
+    adoptDate(dateOfAdopt),
+    feedTime(timeOfFeed)// initialize hireDate
+{
+    // output Employee object to show when constructor is called
+    cout << "Pet object constructor: "
+        << firstName << ' ' << lastName << endl;
+    print();
+} // end Employee constructor
+
+// print Employee object
+void Pet::print() const
+{
+    cout << lastName << ", " << firstName << "  adopt: ";
+    adoptDate.print();
+    adoptDate.printROC();
+    cout << "  Birthday: ";
+    birthDate.print();
+    birthDate.printROC();
+    cout << "  Feedtime: ";
+    feedTime.show();
+    cout << endl;
+} // end function print
+
+// output Employee object to show when its destructor is called
+Pet::~Pet()
+{
+    cout << "Pet object destructor: "
+        << lastName << ", " << firstName << endl;
+} // end ~Employee destructor
