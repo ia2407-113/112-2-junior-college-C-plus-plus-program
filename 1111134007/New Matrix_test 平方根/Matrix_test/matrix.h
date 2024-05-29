88853afd885a9b90fbc057d9807c598333214f82
1111134007tingting <144888945+1111134007tingting@users.noreply.github.com>
2024-05-29 13:36:09 +0800

    HW1 平方根 5/28

---
+++ 1111134007/New Matrix_test 平方根/Matrix_test/matrix.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+#include"matrix.h"
+using namespace std;
+class Matrix
+{
+public:
+    int row, column;
+    int** element;
+
+    Matrix(int m, int n);
+   
+    // 定義矩陣的「-」(負號)前置運算子的多載
+    Matrix operator-();
+   
+};
