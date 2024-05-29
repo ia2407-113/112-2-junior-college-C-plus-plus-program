762db9b141b34601e6e7466c763dab2b836eb76c
Sinyuuuu <144888565+Sinyuuuu@users.noreply.github.com>
2024-05-29 16:08:27 +0800

    Add files via upload

---
+++ 1111134010/W15練習/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+#include "air.h"
+using namespace std;
+
+int main()
+{
+    airbus air1;  // 兼具空中及水上功能   
+    air1.flight_object::display();
+    air1.airliner::display();
+    air1.display();
+    getchar();
+    return 0;
+}
