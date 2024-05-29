b0cff9934d6859ecf7f50a0f6cc83af8f5f367a7
TonyStank9527 <144888937+TonyStank9527@users.noreply.github.com>
2024-05-29 12:45:48 +0800

    Add files via upload

---
+++ 1111134031/0522/ConsoleApplication1/ConsoleApplication1/matrix.h
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Matrix
+{
+     public:
+    int row, column;
+    int** element;
+
+    Matrix(int m, int n);
+    
+
+    // 定義矩陣的「-」(負號)前置運算子的多載
+    Matrix operator-();
+    
+        
+};
---
+++ 1111134031/0522/ConsoleApplication3/ConsoleApplication3/matrix.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include"matrix.h"
+using namespace std;
+
+Matrix:: Matrix(int m, int n)
+    {
+        row = m;
+        column = n;
+
+        // 動態配置mxn的二維陣列element之記憶體
+        element = new int* [m];
+        int i;
+        for (i = 0; i < m; i++)
+            element[i] = new int[n];
+    }
+
+    Matrix::Matrix(char name, int m, int n)
+    {
+        row = m;
+        column = n;
+
+        // 動態配置mxn的二維陣列element之記憶體
+        element = new int* [m];
+        int i, j;
+        for (i = 0; i < m; i++)
+            element[i] = new int[n];
+
+        cout << "輸入一" << m << 'x' << n << "矩陣" << name << endl;
+        for (i = 0; i < m; i++)
+            for (j = 0; j < n; j++)
+            {
+                cout << name << '[' << i << "]["
+                    << j << "]=";
+                cin >> element[i][j];
+            }
+    };
