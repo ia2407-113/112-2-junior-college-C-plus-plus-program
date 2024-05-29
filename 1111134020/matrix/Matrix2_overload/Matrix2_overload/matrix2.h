0245629b4a8473389ca339b973a8c23db17a633d
ghyisjj55555 <109556902+ghyisjj55555@users.noreply.github.com>
2024-05-29 13:14:23 +0800

    0522

---
+++ 1111134020/matrix/Matrix2_overload/Matrix2_overload/matrix2.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+
+class Matrix
+{
+public:
+    int row, column;
+    int** element;
+    Matrix(int m, int n);
+    
+    Matrix operator^(int);
+    
+};
---
+++ 1111134020/matrix/Matrix3_overload/Matrix3_overload/matrix3.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+using namespace std;
+class Matrix
+{
+public:
+    int row, column;
+    int** element;
+    Matrix(int m, int n);
+    Matrix(char name, int m, int n);
+    Matrix operator^(Matrix x);
+
+};
