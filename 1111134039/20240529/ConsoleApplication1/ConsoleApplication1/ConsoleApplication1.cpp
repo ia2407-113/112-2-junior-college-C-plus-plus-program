1b09f849ae49eeb6cb30d1144b9358c866f4d273
Qixuan0116 <144888878+Qixuan0116@users.noreply.github.com>
2024-05-29 16:14:12 +0800

    Add files via upload

---
+++ 1111134039/20240529/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <string>
+#include "shape.h"
+using namespace std;
+int main()
+{
+   cube volume;
+   volume.rectangle::data_input();
+   volume.area();
+   volume.data_input();
+   volume.area();
+    return 0;
+}
---
+++ 1111134039/20240529/ConsoleApplication1/ConsoleApplication1/shape.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include "shape.h"
+using namespace std;  
+void  shape::area()
+{
+    cout << result;
+}
+void  rectangle::data_input()
+{
+    name = "長方體";
+    cout << "計算" << name << "的面積";
+    cout << "輸入" << name << "的長";
+    cin >> length;
+    cout << "輸入" << name << "的寬";
+    cin >> width;
+
+    result = width * length;
+    cout << "面積";
+}
+
+ void  cube::data_input()
+  {
+     name = "長方體";
+     cout << "\n計算" << name << "的面積\n";
+     cout << "輸入" << name << "的長";
+     cin >> length;
+     cout << "輸入" << name << "的寬";
+     cin >> width;
+     cout << "輸入" << name << "的高";
+     cin >> height;
+     result = width * length * height;
+     cout << "面積";
+        }
