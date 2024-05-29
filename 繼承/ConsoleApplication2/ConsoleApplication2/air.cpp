a6c77bc86829d78fbd76e434bd9f35a887070eee
Cyzz11 <144888845+Cyzz11@users.noreply.github.com>
2024-05-29 16:19:35 +0800

    5/29

---
+++ 繼承/ConsoleApplication2/ConsoleApplication2/air.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include<string>
+#include"air.h"
+using namespace std;
+void shape::area()
+{
+	cout << result;
+}
+
+void rectangle::data_input()
+{
+	name = "長方形";
+	cout << "計算" << name << "的面積\n";
+	cout << "輸入" << name << "的長";
+	cin >> lenght;
+	cout << "輸入" << name << "的寬";
+	cin >> width;
+	result = lenght * width;
+	cout << "面積:";
+}
+
+void cube::data_input()
+{
+	name = "長方形";
+	cout << "\n計算" << name << "的體積\n";
+	cout << "輸入" << name << "的長";
+	cin >> lenght;
+	cout << "輸入" << name << "的寬";
+	cin >> width;
+	cout << "輸入" << name << "的高";
+	cin >> height;
+	result = lenght * width*height;
+	cout << "體積:";
+
+}
