4ae7c49ea918b9915bbf476d4f9135c87c486bbd
ccy0118 <144888816+ccy0118@users.noreply.github.com>
2024-05-29 16:14:39 +0800

    05.29

---
+++ 1111134001/ConsoleApplication1/ConsoleApplication1/air.h
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+using namespace std;
+// 飛行物體類別 
+class flight_object
+{
+private:
+    string name;  // 飛行物名稱 
+    int id;       // 飛行物編號 
+    int pilot;    // 駕駛員人數 
+    float kerosene;  // 煤油量(煤油是飛行物使用的燃料) 
+
+public:
+    string manufacturer; //製造者     
+
+    // flight_object類別建構元函式,設定飛行物的資料
+    flight_object();
+    ~flight_object();
+    // 顯示飛行物的資料
+    void display();
+};
+// 大型客機類別
+class airliner :public flight_object
+{
+private:
+    int passenger;  //乘 客人數   
+    int service_person;  // 服務人員的數目 
+
+public:
+    // airliner類別建構元函式,設定大型客機的資料 
+    airliner();
+    ~airliner();
+    // 顯示大型客機的資料
+    void display();
+};
+
+class airbus :public airliner
+{
+private:
+    int bath_room;   // 沐浴設備數目     
+
+public:
+    // airbus類別建構元函式,設定空中巴士客機的資料 
+    airbus();
+    // 顯示空中巴士客機的資料
+    void display();
+};
