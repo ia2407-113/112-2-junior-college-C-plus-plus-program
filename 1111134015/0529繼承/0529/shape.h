cb0b09ced7fd7dd9099f48eb2b1f7d4aaf017e8d
張斯婷 <144889000+zst07@users.noreply.github.com>
2024-05-29 16:02:12 +0800

    Add files via upload

---
+++ 1111134015/0529繼承/0529/shape.h
@@ -0,0 +1,33 @@
+#ifndef shape_H
+#define shape_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class shape 
+{
+public:
+	string name;
+	double result;
+
+	void area();//印出結果
+};
+
+//長方 面
+class rectangle :public shape 
+{
+public:
+	double lenght, width;
+
+	void data_input();
+};
+
+class cube :public rectangle
+{
+public :
+	double height;
+
+	void data_input();
+};
+#endif
