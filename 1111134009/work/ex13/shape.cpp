5a3e6f7c446377e3f385d58528a74035247330d1
1111134009youyun <144888955+1111134009youyun@users.noreply.github.com>
2024-05-29 16:01:57 +0800

    5-29

---
+++ 1111134009/work/ex13/shape.cpp
@@ -0,0 +1,31 @@
+#include "shape.h"
+using namespace std;
+
+#include <iostream>
+#include <string>
+
+void shape::area() {
+	cout << result << endl;
+}
+
+void rectangle::data_input()
+{
+	name = "長方形";
+	cout << "計算" << name << "的面積：\n";
+	cout << "輸入" << name << "的長：";
+	cin >> length;
+	cout << "輸入" << name << "的寬：";
+	cin >> width;
+	result = length * width;
+	cout << "面積:";
+}
+
+void cube::data_input()
+{
+	name = "長方體";
+	cout << "計算" << name << "的面積：\n";
+	cout << "輸入" << name << "的高：";
+	cin >> height;
+	result = length * width * height;
+	cout << "體積:";
+}
