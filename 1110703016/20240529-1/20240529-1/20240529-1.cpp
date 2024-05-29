3cd6d825fc6a0edadee8e76157ce4cba48ca7547
ponpongood <84927301+ponpongood@users.noreply.github.com>
2024-05-29 15:54:29 +0800

    Add files via upload

---
+++ 1110703016/20240529-1/20240529-1/20240529-1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <string>
+#include "air.h"
+
+int main()
+{
+    airbus air1;
+    air1.flight_object::display();
+    air1.airliner::display_airliner();
+    air1.display();
+
+    return 0;
+}
---
+++ 1110703016/20240529-2/20240529-2/20240529-2.cpp
@@ -0,0 +1,17 @@
+﻿// 20240529-2.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+#include <string>
+#include "Header.h"
+using namespace std;
+
+int main() {
+	cube volume;
+	volume.rectangle::data_input();
+	volume.area();
+	volume.data_input();
+	volume.area();
+
+	return 0;
+}
