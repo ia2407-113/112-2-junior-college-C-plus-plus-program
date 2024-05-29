16b80ec4af5ef295eb8795f61af1440178745828
1111134009youyun <144888955+1111134009youyun@users.noreply.github.com>
2024-05-29 18:31:05 +0800

    homework

--- 1111134009/搜尋姓名及電話/ConsoleApplication2/ConsoleApplication2.cpp
+++ 1111134009/搜尋姓名及電話/ConsoleApplication2/ConsoleApplication2.cpp
@@ -1,12 +1,5 @@
 ﻿// ConsoleApplication2.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
-//
 
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
-}
 #include <iostream>
 using namespace std;
 int main()
---
+++ 1111134009/類別名稱/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+
+using namespace std;
+class GradeBook
+{
+public:
+    void CoyrseID() const
+    {
+        cout << "Course ID is: " << ID << endl;
+    }
+
+    void CoyrseTeacher() const
+    {
+        cout << "Course Teacher is: " << Teacher << endl;
+    }
+    void setCourseID(string ID1)
+    {
+        if (ID1 != "")
+            ID = ID1;
+    }
+    void setCourseTeacher(string Teacher1)
+    {
+        if (Teacher1 != "")
+            Teacher = Teacher1;
+    }
+    string getCourseID()
+    {
+     return ID;
+    }
+    string getCourseTeacher()
+    {
+        return Teacher;
+    }
+private:
+    string ID = "1111134007";
+    string Teacher = "侯幸雨";
+}; 
+
+int main()
+{
+    GradeBook myGradeBook;
+    myGradeBook.CoyrseID();
+    myGradeBook.setCourseID("1111134009");
+    myGradeBook.CoyrseTeacher();
+    myGradeBook.setCourseTeacher("陳大仁");
+    myGradeBook.CoyrseID();
+    myGradeBook.CoyrseTeacher();
+    cout << "ID是:" << myGradeBook.getCourseID() << endl ;
+    cout << "Teacher是:" << myGradeBook.getCourseTeacher() << endl;
+} 
