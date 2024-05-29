bacdbe4044d10859918a64fe1178c1eab5534223
Cyzz11 <144888845+Cyzz11@users.noreply.github.com>
2024-05-29 16:23:01 +0800

    5/29

---
+++ 1111134016/繼承/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,17 @@
+﻿// ConsoleApplication1.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+#include <string>
+#include"air.h"
+using namespace std;
+int main()
+{
+    airbus air1;
+    air1.airliner::display();
+    air1.flight_object::display();
+    air1.display();
+    
+
+    return 0;
+}
