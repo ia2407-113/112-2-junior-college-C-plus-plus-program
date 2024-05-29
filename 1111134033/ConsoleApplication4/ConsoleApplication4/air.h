9c17e1c7535a49aeee9c19f986b383d66b75a1f9
tutjui7y6i <144888790+tutjui7y6i@users.noreply.github.com>
2024-05-29 16:17:21 +0800

    Add files via upload

---
+++ 1111134033/ConsoleApplication4/ConsoleApplication4/air.h
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class shape
+{
+public:
+	string name;
+	double result;
+
+	void area();//¦L¥Xµ²ªG
+};
+
+//ªø¤è ­±
+class rectangle :public shape
+{
+public:
+	double lenght, width;
+
+	void data_input();
+};
+
+class cube :public rectangle
+{
+public:
+	double height;
+
+	void data_input();
+};
