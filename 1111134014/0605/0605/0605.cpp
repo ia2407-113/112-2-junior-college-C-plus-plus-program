86ba2fd265c1ba7eedd85ae11eb71d240aeea0af
duyirou <144888576+duyirou@users.noreply.github.com>
2024-06-05 16:40:12 +0800

    分離成實作檔、介面檔、主程式，並且新增輸入後燃器秒數

---
+++ 1111134014/0605/0605/0605.cpp
@@ -0,0 +1,26 @@
+﻿// 0605.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+#include <string>
+#include"air.h"
+using namespace std;
+
+int main()
+{
+    flight_object* flight;
+    airliner air1;
+    battleplane battle1;
+
+    flight = &air1;  // flight指向air1的位址 
+    flight->create_flight_object();
+    air1.create_airliner();
+    flight->display();
+
+    flight = &battle1;  // flight指向battle1的位址 
+    flight->create_flight_object();
+    battle1.create_battleplane();
+    flight->display();
+
+    return 0;
+}
