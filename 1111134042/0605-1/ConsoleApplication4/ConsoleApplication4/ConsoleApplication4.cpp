199775a72f89d6ec67b64898b173a59a7b4d75e1
Hsuan07531 <144888928+Hsuan07531@users.noreply.github.com>
2024-06-05 15:30:22 +0800

    Add files via upload

---
+++ 1111134042/0605-1/ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <string>
+#include "air.h"
+using namespace std;
+int main()
+{
+    flight_object* flight, flight1;
+    airliner air1;
+    flight = &flight1;
+    flight->display();
+
+    flight = &air1;
+    flight->display();
+
+    return 0;
+}
