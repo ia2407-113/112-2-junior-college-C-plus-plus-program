f657f7d935bd17cb2d3315e84de7199cc42fad91
張博鈞 <80932235+2003-David@users.noreply.github.com>
2024-06-05 16:20:58 +0800

    Add files via upload

---
+++ 1110831032/0605/NO1/NO1/NO1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+#include "flight_objects.h"
+
+int main() {
+   
+    //airliner air1;  // 兼具空中及水上功能   
+    //air1.flight_object::display();
+    //air1.display();
+
+    flight_object* flight, flightl;
+    airliner airl;
+
+    flight = &flightl;
+       flight->display();
+
+        flight = &airl;
+        flight->display();
+    return 0;
+}
