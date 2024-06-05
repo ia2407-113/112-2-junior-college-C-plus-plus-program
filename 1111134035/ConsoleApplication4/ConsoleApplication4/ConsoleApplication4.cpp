3cb5c2be8f7f79cd0493bc9f6495f965b24755b9
Nero0227 <144889756+Nero0227@users.noreply.github.com>
2024-06-05 17:12:32 +0800

    Add files via upload

---
+++ 1111134035/ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,40 @@
+﻿// ConsoleApplication4.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+#include"Header.h"
+// 飛行物體類別,是一種抽象類別 
+
+int main()
+{
+    flight_object* flight;
+    airliner air1;
+    battleplane battle1;
+
+    flight = &air1;  // flight指向air1的位址 
+    flight->create_flight_object();
+    air1.create_airliner();
+    flight->display();
+    
+
+    flight = &battle1;  // flight指向battle1的位址 
+    flight->create_flight_object();
+    battle1.create_battleplane();
+    flight->display();
+    
+    return 0;
+}
+
+
+// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
+// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
+
+// 開始使用的提示: 
+//   1. 使用 [方案總管] 視窗，新增/管理檔案
+//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
+//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
+//   4. 使用 [錯誤清單] 視窗，檢視錯誤
+//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
+//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
