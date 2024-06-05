3b12f7e8e2dde59b582e1f46554314532e92225b
Fengxiadddd <155797577+Fengxiadddd@users.noreply.github.com>
2024-06-05 15:20:58 +0800

    Add files via upload

---
+++ 1110934042/w16-/w16-/w16-.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+#include"aaa.h"
+using namespace std;
+// 飛行物體類別 
+
+int main()
+{
+    flight_object* flight, flight1;
+    airliner air1;
+
+    flight = &flight1;
+    flight->display();
+
+    flight = &air1;
+    flight->display();
+    
+    return 0;
+}
