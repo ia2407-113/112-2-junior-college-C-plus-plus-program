34a477cf8a653da8da810b0a715b3406ceb160b9
ccy0118 <144888816+ccy0118@users.noreply.github.com>
2024-06-05 16:45:17 +0800

    06.05練習

---
+++ 1111134001/ConsoleApplication4/ConsoleApplication4/air.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+#include"air.h"
+using namespace std;
+int main()
+{
+	flight_object* flight, flight1;
+	airliner air1;  // 兼具空中及水上功能   
+
+	flight = &flight1;
+	flight->display();
+
+	flight = &air1;
+	flight->display();
+	//air1.flight_object::display();
+	//air1.display();
+
+	return 0;
+}
