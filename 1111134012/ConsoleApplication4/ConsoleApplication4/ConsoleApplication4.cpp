13dd3ba319151102086963e72cc6d78accc08f08
Rodtls <144888506+Rodtls@users.noreply.github.com>
2024-06-05 16:39:36 +0800

    0605-1

---
+++ 1111134012/ConsoleApplication4/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+#include "Header.h"
+int main()
+{
+	flight_object* flight,flight1;
+	airliner air1;
+
+	flight = &flight1;
+	flight->display();
+
+	flight = &air1;
+	flight->display();
+
+
+	//air1.flight_object::display();
+	//air1.display();
+
+	return 0;
+}
