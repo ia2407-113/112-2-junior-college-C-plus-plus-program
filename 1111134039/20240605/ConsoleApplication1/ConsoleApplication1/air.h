971b1c63fcc7a7197cec38bf62b273a921532ff0
Qixuan0116 <144888878+Qixuan0116@users.noreply.github.com>
2024-06-05 17:10:56 +0800

    Add files via upload

---
+++ 1111134039/20240605/ConsoleApplication1/ConsoleApplication1/air.h
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std;
+// 飛行物體類別,是一種抽象類別 
+class flight_object
+{
+public:
+    string name;
+    int id;
+    int pilot;
+    float kerosene;
+    string manufacturer;
+    int afterburner_time;
+    virtual void create_flight_object() = 0;
+    virtual void display() = 0;
+    virtual int afterburner() = 0;
+};
+class airliner :public flight_object
+{
+private:
+    int passenger;
+    int service_person;
+public:
+    void create_airliner();
+    virtual void create_flight_object();
+    virtual void display();
+    virtual int afterburner();
+};
+class battleplane :public flight_object
+{
+private:
+    string weapon;
+
+public:
+    void create_battleplane();
+    virtual void create_flight_object();
+    virtual void display();
+    virtual int afterburner();
+};
