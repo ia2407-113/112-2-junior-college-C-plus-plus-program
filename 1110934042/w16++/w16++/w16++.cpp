67fc1380b4cdd1f9a300893949a87943a923a3d4
Fengxiadddd <155797577+Fengxiadddd@users.noreply.github.com>
2024-06-05 16:07:06 +0800

    Add files via upload

---
+++ 1110934042/w16++/w16++/w16++.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+#include"aaa.h"
+using namespace std;
+// 飛行物體類別,是一種抽象類別 
+
+int main()
+{
+    flight_object* flight;
+    
+    battleplane battle1;
+
+    
+
+    flight = &battle1;  // flight指向battle1的位址 
+    flight->create_flight_object();
+    battle1.create_battleplane();
+    flight->display();
+
+    return 0;
+}
