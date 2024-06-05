38e75a412362d7757eec5d8264fe4fa52135f7de
wanpinggg <144889474+wanpinggg@users.noreply.github.com>
2024-06-05 16:39:42 +0800

    Add files via upload

---
+++ 1111134003/0605 虛擬函數/ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+#include "flight.h"
+using namespace std;
+// 飛行物體類別 
+
+
+int main()
+{
+	flight_object* flight;
+	airliner air1;
+	battleplane battle1;
+
+	flight = &air1;  // flight指向air1的位址 
+	flight->create_flight_object();
+	air1.create_airliner();
+	flight->display();
+
+	flight = &battle1;  // flight指向battle1的位址 
+	flight->create_flight_object();
+	battle1.create_battleplane();
+	flight->display();
+
+	return 0;
+}
