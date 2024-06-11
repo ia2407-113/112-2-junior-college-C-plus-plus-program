61853376cfb624d9e20c80dcc6c56564e2ec9ef3
TonyStank9527 <144888937+TonyStank9527@users.noreply.github.com>
2024-06-11 20:53:16 +0800

    Add files via upload

---
+++ 1111134031/0605/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+#include"Air.h"
+using namespace std;
+// 飛行物體類別 
+
+
+int main()
+{
+    airliner air1;  // 兼具空中及水上功能   
+    flight_object *flight, flight2;
+    flight = &flight2;
+    flight->display();
+
+    flight = &air1;
+    flight->display();
+
+
+    //air1.flight_object::display();
+    //air1.display();
+
+    return 0;
+}
---
+++ 1111134031/0605/ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+#include"Header.h"
+using namespace std;
+// 飛行物體類別,是一種抽象類別 
+
+int main()
+{
+    flight_object* flight;
+    airliner air1;
+    battleplane battle1;
+
+    flight = &air1;  // flight指向air1的位址 
+    flight->create_flight_object();
+    air1.create_airliner();
+    flight->display();
+    flight->afterburner();
+    
+
+    flight = &battle1;  // flight指向battle1的位址 
+    flight->create_flight_object();
+    battle1.create_battleplane();
+    flight->display();
+
+    return 0;
+}
