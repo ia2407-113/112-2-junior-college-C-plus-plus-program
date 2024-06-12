34eb98baf048eefcdcbecd1226d28a723d9464cc
Fengxiadddd <155797577+Fengxiadddd@users.noreply.github.com>
2024-06-12 15:36:03 +0800

    Add files via upload

---
+++ 1110934042/期中準備(1/期中準備/geometry.h
@@ -0,0 +1,43 @@
+#ifndef GEOMETRY_H
+#define GEOMETRY_H
+
+class Circ_1D {
+protected:
+    double radius;
+    const double pi = 3.14;
+
+public:
+    Circ_1D(double r);
+    double compute_circ();
+};
+
+class Pie_2D {
+protected:
+    double area;
+    const double pi = 3.14;
+
+public:
+    Pie_2D(double r);
+    double compute_area();
+};
+
+class Cylinder_3D : public Circ_1D, public Pie_2D {
+protected:
+    double height, volume, surface;
+
+public:
+    Cylinder_3D(double r, double h);
+    double compute_volume();
+    double compute_surface();
+};
+
+class Cron_3D : public Circ_1D {
+protected:
+    double height, volume;
+
+public:
+    Cron_3D(double r, double h);
+    double compute_volume();
+};
+
+#endif
---
+++ 1110934042/期中準備(1/期中準備/期中準備.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "geometry.h"
+
+int main() {
+    double radius, height;
+    std::cout << "輸入半徑和高度： ";
+    std::cin >> radius >> height;
+
+    Cylinder_3D cylinder(radius, height);
+    Cron_3D cone(radius, height);
+
+    std::cout << "底部面積: " << cylinder.compute_area() << std::endl;
+    std::cout << "圓柱體機: " << cylinder.compute_volume() << std::endl;
+    std::cout << "援助面積: " << cylinder.compute_surface() << std::endl;
+    std::cout << "錐體體積: " << cone.compute_volume() << std::endl;
+
+    return 0;
+}
