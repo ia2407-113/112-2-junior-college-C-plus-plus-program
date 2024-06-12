66f40821ba1c7f29afc08bbdbe0b61d306606aca
1111134007tingting <144888945+1111134007tingting@users.noreply.github.com>
2024-06-12 16:16:05 +0800

    0612-2

---
+++ 1111134007/0612-2/0612-2/0612-2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include"circ.h"
+using namespace std;
+
+
+int main() {
+    double radius, height;
+
+    cout << "請輸入圓的半徑: ";
+    cin >> radius;
+
+    pie_2D pie(radius);
+    cout << "圓餅面積: " << pie.getArea();
+
+    cout << "\n請輸入圓柱的高度: ";
+    cin >> height;
+    cylinder_3D cylinder(radius, height);
+    cout << "圓柱體積: " << cylinder.getVolume();
+    cout << "\n圓柱表面積: " << cylinder.getSurfaceArea();
+
+    cout << "\n請輸入圓錐的高度: ";
+    cin >> height;
+    cron_3D cone(radius, height);
+    cout << "圓錐體積: " << cone.getVolume();
+
+    return 0;
+}
